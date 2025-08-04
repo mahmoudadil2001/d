c82a460cbed17569a90bc4a8abccd6b1bdd7ccaf
mahmoud adil <mahmod.adil2001@gmail.com>
2025-08-04 20:48:30 +0300

    Update lectureNames.js

--- lectureNames.js
+++ lectureNames.js
@@ -32,7 +32,7 @@ export const lectureNames = {
     26: "Unknown", 27: "Unknown", 28: "Unknown", 29: "Unknown", 30: "Unknown"
   },
   oralpathology: {
-    1: "Unknown", 2: "Unknown", 3: "Unknown", 4: "Unknown", 5: "Unknown",
+    1: "introduction", 2: "Unknown", 3: "Unknown", 4: "Unknown", 5: "Unknown",
     6: "Unknown", 7: "Unknown", 8: "Unknown", 9: "Unknown", 10: "Unknown",
     11: "Unknown", 12: "Unknown", 13: "Unknown", 14: "Unknown", 15: "Unknown",
     16: "Unknown", 17: "Unknown", 18: "Unknown", 19: "Unknown", 20: "Unknown",
