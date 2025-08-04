bb73447c40106aa7d1e0775264359efcbbcc0523
mahmoud adil <mahmod.adil2001@gmail.com>
2025-08-04 17:45:59 +0300

    Update show.js

--- show.js
+++ show.js
@@ -1,44 +1,33 @@
-// show.js
-
 export const visibleLectures = {
   endodontics: {
-    1: ['v1', 'v2', 'v3'],   // محاضرة 1 فيها نسخ v1 و v2 و v3 تظهر للاختبار
-    2: ['v1'],               // محاضرة 2 فيها نسخة واحدة v1
-    3: ['v1', 'v4'],
-    10: ['v2'],
+    1: [1, 2],
+    2: [1],
   },
   generalmedicine: {
-    1: ['v1', 'v3'],
-    5: ['v2'],
-    12: ['v1', 'v2', 'v4'],
+    1: [1]
   },
   generalsurgery: {
-    1: ['v1'],
-    2: ['v2', 'v3'],
-    7: ['v1'],
+    1: [1],
   },
   operative: {
-    1: ['v1', 'v2'],
-    3: ['v3'],
+    1: [1],
   },
   oralpathology: {
-    1: ['v1', 'v4'],
-    8: ['v2'],
+    1: [1],
   },
   oralsurgery: {
-    2: ['v1', 'v2', 'v3', 'v4'],
+    1: [1],
   },
   orthodontics: {
-    1: ['v1'],
-    10: ['v2', 'v3'],
+    1: [1],
   },
   pedodontics: {
-    4: ['v1'],
+    1: [1],
   },
   periodontology: {
-    1: ['v1', 'v3'],
+    1: [1],
   },
   prosthodontics: {
-    5: ['v2', 'v3'],
+    1: [1],
   },
 };
