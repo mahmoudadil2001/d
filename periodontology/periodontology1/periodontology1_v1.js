eefd2241bf130b05c21b38c4ea4474fdfde9bce6
mahmoud adil <mahmod.adil2001@gmail.com>
2025-08-06 10:12:38 +0300

    Update periodontology1_v1.js

--- periodontology/periodontology1/periodontology1_v1.js
+++ periodontology/periodontology1/periodontology1_v1.js
@@ -1,7 +1,252 @@
 export const questions = [
   {
-    question: "Sample question for Periodontology Lecture 1, Version 1?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    question: "What does the term 'periodontium' mean?",
+    options: [
+      "The tissues around and supporting the teeth",
+      "The study of gum diseases",
+      "The treatment of periodontal diseases",
+      "The branch of dentistry dealing with tooth extraction"
+    ],
     answer: 0
+  },
+  {
+    question: "Which of the following is NOT a component of the periodontium?",
+    options: [
+      "Alveolar bone",
+      "Root cementum",
+      "Dentin",
+      "Periodontal ligament"
+    ],
+    answer: 2
+  },
+  {
+    question: "What is the primary function of the gingiva?",
+    options: [
+      "To produce saliva",
+      "To protect surrounding tissues from the oral environment",
+      "To anchor teeth to the jawbone",
+      "To secrete enamel"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which type of mucosa includes the gingiva and the covering of the hard palate?",
+    options: [
+      "Lining mucosa",
+      "Specialized mucosa",
+      "Masticatory mucosa",
+      "Alveolar mucosa"
+    ],
+    answer: 2
+  },
+  {
+    question: "Where is the mucogingival junction located?",
+    options: [
+      "Between the free gingiva and attached gingiva",
+      "Between the attached gingiva and alveolar mucosa",
+      "Between the alveolar mucosa and the vestibule",
+      "On the palatal side of the gingiva"
+    ],
+    answer: 1
+  },
+  {
+    question: "What is the normal probing depth of a healthy gingival sulcus?",
+    options: [
+      "0 mm",
+      "1-2 mm",
+      "2-3 mm",
+      "4-5 mm"
+    ],
+    answer: 2
+  },
+  {
+    question: "Which part of the gingiva is firmly attached to the tooth and underlying bone?",
+    options: [
+      "Marginal gingiva",
+      "Attached gingiva",
+      "Interdental gingiva",
+      "Alveolar mucosa"
+    ],
+    answer: 1
+  },
+  {
+    question: "What is the shape of the interdental gingiva when teeth are in contact?",
+    options: [
+      "Col",
+      "Pyramidal",
+      "Circular",
+      "Linear"
+    ],
+    answer: 0
+  },
+  {
+    question: "Which type of epithelium covers the outer surface of the gingiva?",
+    options: [
+      "Sulcular epithelium",
+      "Junctional epithelium",
+      "Oral epithelium",
+      "Non-keratinized epithelium"
+    ],
+    answer: 2
+  },
+  {
+    question: "Which layer of the oral epithelium is responsible for cell renewal?",
+    options: [
+      "Stratum corneum",
+      "Stratum granulosum",
+      "Stratum spinosum",
+      "Stratum basale"
+    ],
+    answer: 3
+  },
+  {
+    question: "What is the primary cell type in the oral epithelium?",
+    options: [
+      "Melanocytes",
+      "Keratinocytes",
+      "Langerhans cells",
+      "Merkel cells"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which epithelium forms the base of the gingival sulcus?",
+    options: [
+      "Oral epithelium",
+      "Sulcular epithelium",
+      "Junctional epithelium",
+      "Alveolar epithelium"
+    ],
+    answer: 2
+  },
+  {
+    question: "What is the turnover rate of the junctional epithelium?",
+    options: [
+      "1-2 days",
+      "4-6 days",
+      "6-12 days",
+      "20-30 days"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which of the following is NOT a principal group of gingival fibers?",
+    options: [
+      "Dentogingival fibers",
+      "Alveolar gingival fibers",
+      "Trans-septal fibers",
+      "Elastin fibers"
+    ],
+    answer: 3
+  },
+  {
+    question: "What is the main function of the dentogingival fibers?",
+    options: [
+      "To attach the gingiva to the alveolar bone",
+      "To provide support to the gingiva by attaching it to the tooth",
+      "To protect the periodontal ligament",
+      "To maintain tooth-to-tooth contact"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which cells are responsible for producing melanin in the gingiva?",
+    options: [
+      "Keratinocytes",
+      "Melanocytes",
+      "Langerhans cells",
+      "Merkel cells"
+    ],
+    answer: 1
+  },
+  {
+    question: "What is the normal color of healthy gingiva?",
+    options: [
+      "Bright red",
+      "Bluish red",
+      "Coral pink",
+      "White"
+    ],
+    answer: 2
+  },
+  {
+    question: "Which of the following is a characteristic of inflamed gingiva?",
+    options: [
+      "Coral pink color",
+      "Knife-edged margins",
+      "Bluish red color",
+      "Stippled surface"
+    ],
+    answer: 2
+  },
+  {
+    question: "What gives the attached gingiva its stippled appearance?",
+    options: [
+      "Keratinization",
+      "Alternating pattern of connective tissue papillae and epithelial rete pegs",
+      "Presence of melanin",
+      "High vascularity"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which of the following is NOT a layer of the basement membrane?",
+    options: [
+      "Lamina lucida",
+      "Lamina densa",
+      "Stratum corneum",
+      "Anchoring fibrils"
+    ],
+    answer: 2
+  },
+  {
+    question: "What is the primary component of the gingival connective tissue fibers?",
+    options: [
+      "Collagen",
+      "Elastin",
+      "Oxytalan",
+      "Reticulin"
+    ],
+    answer: 0
+  },
+  {
+    question: "Which of the following is a function of the gingival fibers?",
+    options: [
+      "To produce saliva",
+      "To withstand forces exerted by mastication",
+      "To secrete enamel",
+      "To anchor teeth to the jawbone"
+    ],
+    answer: 1
+  },
+  {
+    question: "Where is the width of the attached gingiva the greatest?",
+    options: [
+      "Mandibular first premolar area",
+      "Maxillary incisors region",
+      "Mandibular molar region",
+      "Maxillary canine region"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which of the following is true about the sulcular epithelium?",
+    options: [
+      "It is keratinized",
+      "It has rete pegs",
+      "It may act as a semi-permeable membrane",
+      "It is the most keratinized part of the gingiva"
+    ],
+    answer: 2
+  },
+  {
+    question: "What is the main function of the junctional epithelium?",
+    options: [
+      "To produce keratin",
+      "To attach the gingiva to the tooth surface",
+      "To secrete saliva",
+      "To withstand masticatory forces"
+    ],
+    answer: 1
   }
 ];
