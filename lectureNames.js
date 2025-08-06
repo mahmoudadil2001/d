ccbcf2ab7daf8c57de902c3965372306d6c7375c
mahmoud adil <mahmod.adil2001@gmail.com>
2025-08-06 10:09:28 +0300

    Update lectureNames.js

--- lectureNames.js
+++ lectureNames.js
@@ -64,7 +64,7 @@ export const lectureNames = {
     26: "Unknown", 27: "Unknown", 28: "Unknown", 29: "Unknown", 30: "Unknown"
   },
   periodontology: {
-    1: "Unknown", 2: "Unknown", 3: "Unknown", 4: "Unknown", 5: "Unknown",
+    1: "Terms in periodontology", 2: "Unknown", 3: "Unknown", 4: "Unknown", 5: "Unknown",
     6: "Unknown", 7: "Unknown", 8: "Unknown", 9: "Unknown", 10: "Unknown",
     11: "Unknown", 12: "Unknown", 13: "Unknown", 14: "Unknown", 15: "Unknown",
     16: "Unknown", 17: "Unknown", 18: "Unknown", 19: "Unknown", 20: "Unknown",
@@ -79,4 +79,5 @@ export const lectureNames = {
     21: "Unknown", 22: "Unknown", 23: "Unknown", 24: "Unknown", 25: "Unknown",
     26: "Unknown", 27: "Unknown", 28: "Unknown", 29: "Unknown", 30: "Unknown"
   }
+
 };
