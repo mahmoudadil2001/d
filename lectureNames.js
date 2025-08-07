c1b1d30b7647d1c158004adc39baa80e73112ea3
mahmoud adil <mahmod.adil2001@gmail.com>
2025-08-07 21:20:36 +0300

    Update lectureNames.js

--- lectureNames.js
+++ lectureNames.js
@@ -1,6 +1,6 @@
 export const lectureNames = {
   endodontics: {
-    1: "introduction", 2: "Unknown", 3: "Unknown", 4: "Unknown", 5: "Unknown",
+    1: "introduction", 2: "Classification of periapical disease", 3: "Unknown", 4: "Unknown", 5: "Unknown",
     6: "Unknown", 7: "Unknown", 8: "Unknown", 9: "Unknown", 10: "Unknown",
     11: "Unknown", 12: "Unknown", 13: "Unknown", 14: "Unknown", 15: "Unknown",
     16: "Unknown", 17: "Unknown", 18: "Unknown", 19: "Unknown", 20: "Unknown",
@@ -79,4 +79,5 @@ export const lectureNames = {
     21: "Unknown", 22: "Unknown", 23: "Unknown", 24: "Unknown", 25: "Unknown",
     26: "Unknown", 27: "Unknown", 28: "Unknown", 29: "Unknown", 30: "Unknown"
   }
+
 };
