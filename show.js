c0e1453d0a4ae646b7cb1a5a237f8090120ec9cb
mahmoud adil <mahmod.adil2001@gmail.com>
2025-08-08 18:50:58 +0300

    Add files via upload

---
+++ show.js
@@ -0,0 +1,33 @@
+export const visibleLectures = {
+  endodontics: {
+    1: [1, 2],
+  },
+  generalmedicine: {
+    1: [1, 2, 3, 4],
+    2: [1, 2, 3, 4],
+  },
+  generalsurgery: {
+    1: [1],
+  },
+  operative: {
+    1: [1],
+  },
+  oralpathology: {
+    1: [1],
+  },
+  oralsurgery: {
+    1: [1],
+  },
+  orthodontics: {
+    1: [1],
+  },
+  pedodontics: {
+    1: [1],
+  },
+  periodontology: {
+    1: [1],
+  },
+  prosthodontics: {
+  1: [1],
+  }
+};
