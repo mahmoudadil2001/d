f6b6a3cd09df28e5316bc10af16670434f736066
mahmoud adil <mahmod.adil2001@gmail.com>
2025-08-09 07:06:06 +0300

    Update visibility for endodontics lecture 1 version 1

--- show.js
+++ show.js
@@ -1,33 +1,61 @@
 export const visibleLectures = {
-  endodontics: {
-    1: [1, 2],
+  "endodontics": {
+    "1": [
+      2
+    ]
   },
-  generalmedicine: {
-    1: [1, 2, 3, 4],
-    2: [1, 2, 3, 4],
+  "generalmedicine": {
+    "1": [
+      1,
+      2,
+      3,
+      4
+    ],
+    "2": [
+      1,
+      2,
+      3,
+      4
+    ]
   },
-  generalsurgery: {
-    1: [1],
+  "generalsurgery": {
+    "1": [
+      1
+    ]
   },
-  operative: {
-    1: [1],
+  "operative": {
+    "1": [
+      1
+    ]
   },
-  oralpathology: {
-    1: [1],
+  "oralpathology": {
+    "1": [
+      1
+    ]
   },
-  oralsurgery: {
-    1: [1],
+  "oralsurgery": {
+    "1": [
+      1
+    ]
   },
-  orthodontics: {
-    1: [1],
+  "orthodontics": {
+    "1": [
+      1
+    ]
   },
-  pedodontics: {
-    1: [1],
+  "pedodontics": {
+    "1": [
+      1
+    ]
   },
-  periodontology: {
-    1: [1],
+  "periodontology": {
+    "1": [
+      1
+    ]
   },
-  prosthodontics: {
-  1: [1],
+  "prosthodontics": {
+    "1": [
+      1
+    ]
   }
 };
