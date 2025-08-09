df52ca39dfa87453b01a2d2d756c0edf2aca8c94
mahmoud adil <mahmod.adil2001@gmail.com>
2025-08-09 11:10:20 +0300

    Update show.js

--- show.js
+++ show.js
@@ -1,63 +1,37 @@
 export const visibleLectures = {
     "endodontics": {
-        "1": [
-            2,
-            1
-        ],
-        ],
+        "1": [2,1],
         "4": [],
         "3x": []
     },
     "generalmedicine": {
-        "1": [
-            1
-        ],
-        "2": [
-            1
-        ]
+        "1": [1],
+        "2": [1]
     },
     "generalsurgery": {
-        "1": [
-            1
-        ],
-        "29": [
-            4
-        ]
+        "1": [1],
+        "29": [4]
     },
     "operative": {
         "0": [],
-        "1": [
-            1
-        ]
+        "1": [1]
     },
     "oralpathology": {
-        "1": [
-            1
-        ]
+        "1": [1]
     },
     "oralsurgery": {
-        "1": [
-            1
-        ]
+        "1": [1]
     },
     "orthodontics": {
-        "1": [
-            1
-        ]
+        "1": [1]
     },
     "pedodontics": {
-        "1": [
-            1
-        ]
+        "1": [1]
     },
     "periodontology": {
-        "1": [
-            1
-        ]
+        "1": [1]
     },
     "prosthodontics": {
-        "1": [
-            1
-        ]
+        "1": [1]
     }
 };
