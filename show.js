9b19da6603b1c688aeb8a044a2734eb2b77c5bc1
mahmoud adil <mahmod.adil2001@gmail.com>
2025-08-09 09:36:46 +0300

    Toggle visibility for endodontics lecture 1 version 1

--- show.js
+++ show.js
@@ -1,67 +1,66 @@
 export const visibleLectures = {
-  "endodontics": {
-    "1": [
-      2,
-      1
-    ],
-    "3": [],
-    "4": []
-  },
-  "generalmedicine": {
-    "1": [
-      1,
-      2,
-      3,
-      4
-    ],
-    "2": [
-      1,
-      2,
-      3,
-      4
-    ]
-  },
-  "generalsurgery": {
-    "1": [
-      1
-    ],
-    "29": [
-      4
-    ]
-  },
-  "operative": {
-    "1": [
-      1
-    ]
-  },
-  "oralpathology": {
-    "1": [
-      1
-    ]
-  },
-  "oralsurgery": {
-    "1": [
-      1
-    ]
-  },
-  "orthodontics": {
-    "1": [
-      1
-    ]
-  },
-  "pedodontics": {
-    "1": [
-      1
-    ]
-  },
-  "periodontology": {
-    "1": [
-      1
-    ]
-  },
-  "prosthodontics": {
-    "1": [
-      1
-    ]
-  }
+    "endodontics": {
+        "1": [
+            2
+        ],
+        "3": [],
+        "4": []
+    },
+    "generalmedicine": {
+        "1": [
+            1,
+            2,
+            3,
+            4
+        ],
+        "2": [
+            1,
+            2,
+            3,
+            4
+        ]
+    },
+    "generalsurgery": {
+        "1": [
+            1
+        ],
+        "29": [
+            4
+        ]
+    },
+    "operative": {
+        "1": [
+            1
+        ]
+    },
+    "oralpathology": {
+        "1": [
+            1
+        ]
+    },
+    "oralsurgery": {
+        "1": [
+            1
+        ]
+    },
+    "orthodontics": {
+        "1": [
+            1
+        ]
+    },
+    "pedodontics": {
+        "1": [
+            1
+        ]
+    },
+    "periodontology": {
+        "1": [
+            1
+        ]
+    },
+    "prosthodontics": {
+        "1": [
+            1
+        ]
+    }
 };
