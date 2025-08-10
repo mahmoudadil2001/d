0986c56dd325b26e812fc804bb4228fd98544578
mahmoud adil <mahmod.adil2001@gmail.com>
2025-08-11 01:21:45 +0300

    Update content for periodontology1_v2.js

--- periodontology/periodontology1/periodontology1_v2.js
+++ periodontology/periodontology1/periodontology1_v2.js
@@ -1,7 +1,252 @@
 export const questions = [
   {
-    question: "Sample question for Periodontology Lecture 1, Version 2?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    question: "Which of the following tissues is part of the periodontium?",
+    options: [
+      "Dental pulp",
+      "Enamel",
+      "Periodontal ligament",
+      "Dentin"
+    ],
+    answer: 2
+  },
+  {
+    question: "What is the term for the treatment of periodontal diseases?",
+    options: [
+      "Periodontology",
+      "Periodontal therapy",
+      "Periodontics",
+      "Endodontics"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which type of mucosa covers the dorsum of the tongue?",
+    options: [
+      "Masticatory mucosa",
+      "Specialized mucosa",
+      "Lining mucosa",
+      "Alveolar mucosa"
+    ],
+    answer: 1
+  },
+  {
+    question: "What is the name of the epithelium that attaches the gingiva to the tooth surface?",
+    options: [
+      "Oral epithelium",
+      "Sulcular epithelium",
+      "Junctional epithelium",
+      "Masticatory epithelium"
+    ],
+    answer: 2
+  },
+  {
+    question: "Which of the following is a characteristic of the sulcular epithelium?",
+    options: [
+      "Highly keratinized",
+      "Contains rete pegs",
+      "Non-keratinized and thin",
+      "Attached to the tooth surface"
+    ],
+    answer: 2
+  },
+  {
+    question: "What is the primary function of the gingival fibers?",
+    options: [
+      "To produce saliva",
+      "To support and attach the gingiva to the tooth and bone",
+      "To secrete enamel",
+      "To form the col"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which of the following is the most abundant cell type in the gingival connective tissue?",
+    options: [
+      "Mast cells",
+      "Fibroblasts",
+      "Macrophages",
+      "Lymphocytes"
+    ],
+    answer: 1
+  },
+  {
+    question: "What is the main component of the connective tissue fibers?",
+    options: [
+      "Elastin",
+      "Collagen",
+      "Reticulin",
+      "Oxytalan"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which of the following describes the color of inflamed gingiva?",
+    options: [
+      "Coral pink",
+      "Bluish red",
+      "White",
+      "Yellow"
+    ],
+    answer: 1
+  },
+  {
+    question: "What is the shape of healthy gingival margins?",
+    options: [
+      "Rounded and enlarged",
+      "Knife-edged and scalloped",
+      "Irregular and jagged",
+      "Flat and smooth"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which of the following is a feature of the attached gingiva?",
+    options: [
+      "Dark red color",
+      "Movable tissue",
+      "Firm and resilient",
+      "Non-keratinized"
+    ],
+    answer: 2
+  },
+  {
+    question: "What is the term for the space between the tooth and the free gingiva?",
+    options: [
+      "Gingival groove",
+      "Gingival sulcus",
+      "Col",
+      "Mucogingival junction"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which of the following is NOT a layer of the oral epithelium?",
+    options: [
+      "Stratum basale",
+      "Stratum spinosum",
+      "Stratum granulosum",
+      "Stratum lucidum"
+    ],
+    answer: 3
+  },
+  {
+    question: "What is the function of the trans-septal fibers?",
+    options: [
+      "To attach the gingiva to the tooth",
+      "To connect adjacent teeth",
+      "To protect the periodontal ligament",
+      "To support the alveolar bone"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which of the following is a characteristic of the junctional epithelium?",
+    options: [
+      "Highly keratinized",
+      "Slow turnover rate",
+      "Few desmosomes",
+      "Thick and dense"
+    ],
+    answer: 2
+  },
+  {
+    question: "What is the primary role of the melanocytes in the gingiva?",
+    options: [
+      "To produce keratin",
+      "To synthesize melanin",
+      "To defend against bacteria",
+      "To provide tactile sensation"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which of the following is a component of the dentoperiodontal unit?",
+    options: [
+      "Enamel",
+      "Dental pulp",
+      "Periodontium",
+      "Dentin"
+    ],
+    answer: 2
+  },
+  {
+    question: "What is the term for the junction between the attached gingiva and the alveolar mucosa?",
+    options: [
+      "Gingival sulcus",
+      "Mucogingival junction",
+      "Free gingival groove",
+      "Col"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which of the following describes the surface texture of healthy attached gingiva?",
+    options: [
+      "Smooth and shiny",
+      "Stippled",
+      "Rough and irregular",
+      "Soft and spongy"
+    ],
+    answer: 1
+  },
+  {
+    question: "What is the primary function of the alveolar gingival fibers?",
+    options: [
+      "To attach the gingiva to the alveolar bone",
+      "To connect adjacent teeth",
+      "To protect the periodontal ligament",
+      "To support the free gingiva"
+    ],
     answer: 0
+  },
+  {
+    question: "Which of the following is a characteristic of the col?",
+    options: [
+      "Highly keratinized",
+      "Valley-like depression",
+      "Found in the attached gingiva",
+      "Present in all interdental areas"
+    ],
+    answer: 1
+  },
+  {
+    question: "What is the primary source of blood supply to the gingiva?",
+    options: [
+      "Internal maxillary artery",
+      "Facial artery",
+      "Lingual artery",
+      "Mandibular artery"
+    ],
+    answer: 0
+  },
+  {
+    question: "Which of the following is a feature of the lining mucosa?",
+    options: [
+      "Firmly attached to bone",
+      "Covers the hard palate",
+      "Non-keratinized and movable",
+      "Highly keratinized"
+    ],
+    answer: 2
+  },
+  {
+    question: "What is the term for the shallow linear depression separating the marginal and attached gingiva?",
+    options: [
+      "Gingival sulcus",
+      "Free gingival groove",
+      "Mucogingival junction",
+      "Col"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which of the following is a characteristic of inflamed gingiva?",
+    options: [
+      "Coral pink color",
+      "Knife-edged margins",
+      "Reduced stippling",
+      "Firm consistency"
+    ],
+    answer: 2
   }
 ];
