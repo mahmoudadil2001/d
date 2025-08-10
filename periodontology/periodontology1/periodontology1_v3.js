0be98762efe3cb87a9adf0cbf272411256827215
mahmoud adil <mahmod.adil2001@gmail.com>
2025-08-11 01:22:16 +0300

    Update content for periodontology1_v3.js

--- periodontology/periodontology1/periodontology1_v3.js
+++ periodontology/periodontology1/periodontology1_v3.js
@@ -1,7 +1,252 @@
 export const questions = [
   {
-    question: "Sample question for Periodontology Lecture 1, Version 3?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
-    answer: 0
+    question: "What is the term for the branch of dentistry concerned with the prevention and treatment of periodontal disease?",
+    options: [
+      "Periodontology",
+      "Periodontal therapy",
+      "Periodontics",
+      "Endodontics"
+    ],
+    answer: 2
+  },
+  {
+    question: "Which of the following is NOT a supporting tissue of the periodontium?",
+    options: [
+      "Alveolar bone",
+      "Root cementum",
+      "Dental pulp",
+      "Periodontal ligament"
+    ],
+    answer: 2
+  },
+  {
+    question: "What is the primary function of the masticatory mucosa?",
+    options: [
+      "To secrete saliva",
+      "To withstand frictional forces during mastication",
+      "To produce melanin",
+      "To provide tactile sensation"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which of the following describes the alveolar mucosa?",
+    options: [
+      "Firmly attached to bone",
+      "Keratinized epithelium",
+      "Dark red and movable",
+      "Stippled surface"
+    ],
+    answer: 2
+  },
+  {
+    question: "What is the term for the epithelium that lines the gingival sulcus?",
+    options: [
+      "Oral epithelium",
+      "Sulcular epithelium",
+      "Junctional epithelium",
+      "Masticatory epithelium"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which of the following is a characteristic of the junctional epithelium?",
+    options: [
+      "Highly keratinized",
+      "Slow turnover rate",
+      "Forms attachment to the tooth",
+      "Contains rete pegs"
+    ],
+    answer: 2
+  },
+  {
+    question: "What is the primary cell type in the oral epithelium?",
+    options: [
+      "Melanocytes",
+      "Keratinocytes",
+      "Langerhans cells",
+      "Merkel cells"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which of the following fibers connect the cementum to the alveolar bone?",
+    options: [
+      "Dentogingival fibers",
+      "Trans-septal fibers",
+      "Dentoperiosteal fibers",
+      "Circular fibers"
+    ],
+    answer: 2
+  },
+  {
+    question: "What is the main constituent of the connective tissue ground substance?",
+    options: [
+      "Collagen fibers",
+      "Proteoglycans and glycoproteins",
+      "Elastin fibers",
+      "Reticulin fibers"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which of the following describes the color of healthy gingiva?",
+    options: [
+      "Bright red",
+      "Bluish red",
+      "Coral pink",
+      "White"
+    ],
+    answer: 2
+  },
+  {
+    question: "What is the term for the valley-like depression in the interdental gingiva?",
+    options: [
+      "Gingival sulcus",
+      "Col",
+      "Free gingival groove",
+      "Mucogingival junction"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which of the following is a feature of inflamed gingiva?",
+    options: [
+      "Knife-edged margins",
+      "Coral pink color",
+      "Bluish red color",
+      "Stippled surface"
+    ],
+    answer: 2
+  },
+  {
+    question: "What is the primary function of the dentogingival fibers?",
+    options: [
+      "To attach the gingiva to the alveolar bone",
+      "To connect adjacent teeth",
+      "To support the gingiva by attaching it to the tooth",
+      "To protect the periodontal ligament"
+    ],
+    answer: 2
+  },
+  {
+    question: "Which layer of the oral epithelium is responsible for cell renewal?",
+    options: [
+      "Stratum corneum",
+      "Stratum granulosum",
+      "Stratum spinosum",
+      "Stratum basale"
+    ],
+    answer: 3
+  },
+  {
+    question: "What is the shape of the interdental gingiva in the presence of diastema?",
+    options: [
+      "Pyramidal",
+      "Col",
+      "Absent",
+      "Circular"
+    ],
+    answer: 2
+  },
+  {
+    question: "Which cells are responsible for producing melanin in the gingiva?",
+    options: [
+      "Keratinocytes",
+      "Melanocytes",
+      "Langerhans cells",
+      "Merkel cells"
+    ],
+    answer: 1
+  },
+  {
+    question: "What is the primary function of the periodontal ligament?",
+    options: [
+      "To secrete enamel",
+      "To connect the cementum to the alveolar bone",
+      "To produce saliva",
+      "To protect the pulp"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which of the following is NOT a principal group of gingival fibers?",
+    options: [
+      "Dentogingival fibers",
+      "Trans-septal fibers",
+      "Dentoperiosteal fibers",
+      "Pulpoperiosteal fibers"
+    ],
+    answer: 3
+  },
+  {
+    question: "What is the width of the attached gingiva greatest in?",
+    options: [
+      "Mandibular premolars",
+      "Maxillary incisors",
+      "Mandibular molars",
+      "Maxillary molars"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which of the following is a characteristic of the alveolar mucosa?",
+    options: [
+      "Firmly attached to bone",
+      "Keratinized epithelium",
+      "Dark red and movable",
+      "Stippled surface"
+    ],
+    answer: 2
+  },
+  {
+    question: "What is the primary role of the junctional epithelium?",
+    options: [
+      "To produce keratin",
+      "To attach the gingiva to the tooth surface",
+      "To secrete saliva",
+      "To form the col"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which of the following describes the consistency of healthy gingiva?",
+    options: [
+      "Soft and spongy",
+      "Resilient and firm",
+      "Flaccid and loose",
+      "Hard and calcified"
+    ],
+    answer: 1
+  },
+  {
+    question: "What is the term for the junction between the free and attached gingiva?",
+    options: [
+      "Gingival sulcus",
+      "Free gingival groove",
+      "Mucogingival junction",
+      "Col"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which of the following is a characteristic of the sulcular epithelium?",
+    options: [
+      "Highly keratinized",
+      "Contains rete pegs",
+      "Non-keratinized and thin",
+      "Attached to the tooth surface"
+    ],
+    answer: 2
+  },
+  {
+    question: "What is the primary function of the gingival fibers?",
+    options: [
+      "To produce saliva",
+      "To support and attach the gingiva to the tooth and bone",
+      "To secrete enamel",
+      "To form the col"
+    ],
+    answer: 1
   }
 ];
