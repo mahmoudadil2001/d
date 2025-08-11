1dc20c543aee87697bd46d737491ff21066ddc97
mahmoud adil <mahmod.adil2001@gmail.com>
2025-08-11 16:37:57 +0300

    Toggle whole lecture visibility for endodontics lecture 2

--- show.js
+++ show.js
@@ -6,10 +6,10 @@ export const visibleLectures = {
             3,
             4
         ],
-        "four": [],
-        "two": [
+        "2": [
             1
-        ]
+        ],
+        "four": []
     },
     "generalmedicine": {
         "1": [
