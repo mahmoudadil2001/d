8d62609b036b86a90704db03ad60b76d269018a9
mahmoud adil <mahmod.adil2001@gmail.com>
2025-08-13 11:24:41 +0300

    Update content for operative22_v1.js

--- operative/operative22/operative22_v1.js
+++ operative/operative22/operative22_v1.js
@@ -1,7 +1,7 @@
 export const questions = [
   {
     question: "Sample question for Operative Lecture 22, Version 1?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    options: ["Option ", "Option 2", "Option 3", "Option 4"],
     answer: 0
   }
 ];
