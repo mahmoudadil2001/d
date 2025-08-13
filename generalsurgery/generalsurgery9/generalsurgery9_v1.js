b53b69fec49cc0881a04443b00f9076b2c2b868b
mahmoud adil <mahmod.adil2001@gmail.com>
2025-08-13 10:32:07 +0300

    Update content for generalsurgery9_v1.js

--- generalsurgery/generalsurgery9/generalsurgery9_v1.js
+++ generalsurgery/generalsurgery9/generalsurgery9_v1.js
@@ -1,7 +1,7 @@
 export const questions = [
   {
     question: "Sample question for Generalsurgery Lecture 9, Version 1?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    options: ["Option ", "Option 2", "Option 3", "Option 4"],
     answer: 0
   }
 ];
