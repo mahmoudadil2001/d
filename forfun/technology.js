ae07a14a3ccb0da360a30230cf5068445b35c91d
mahmoud adil <mahmod.adil2001@gmail.com>
2025-08-14 12:57:26 +0300

    Add files via upload

---
+++ forfun/technology.js
@@ -0,0 +1,53 @@
+
+export const questions = [
+  {
+    question: "من أسس شركة مايكروسوفت؟",
+    options: ["ستيف جوبز", "بيل غيتس", "مارك زوكربيرج", "لاري بيج"],
+    answer: 1
+  },
+  {
+    question: "ما معنى 'WWW' في عناوين المواقع؟",
+    options: ["World Wide Web", "World Web Work", "Web World Wide", "Wide World Web"],
+    answer: 0
+  },
+  {
+    question: "في أي عام تم إطلاق أول آيفون؟",
+    options: ["2005", "2006", "2007", "2008"],
+    answer: 2
+  },
+  {
+    question: "ما هو نظام التشغيل الذي طورته جوجل للهواتف؟",
+    options: ["iOS", "Windows Phone", "Android", "BlackBerry"],
+    answer: 2
+  },
+  {
+    question: "كم بايت في الكيلوبايت الواحد؟",
+    options: ["1000", "1024", "1048", "1056"],
+    answer: 1
+  },
+  {
+    question: "ما هو أشهر متصفح إنترنت حالياً؟",
+    options: ["Internet Explorer", "Firefox", "Chrome", "Safari"],
+    answer: 2
+  },
+  {
+    question: "من اخترع شبكة الإنترنت؟",
+    options: ["تيم بيرنرز لي", "بيل غيتس", "ستيف جوبز", "مارك زوكربيرج"],
+    answer: 0
+  },
+  {
+    question: "ما هي أول شبكة اجتماعية في العالم؟",
+    options: ["MySpace", "Friendster", "SixDegrees", "Facebook"],
+    answer: 2
+  },
+  {
+    question: "ما معنى 'CPU' في الحاسوب؟",
+    options: ["Central Processing Unit", "Computer Personal Unit", "Central Program Unit", "Computer Processing Unit"],
+    answer: 0
+  },
+  {
+    question: "أي شركة طورت لغة البرمجة Java؟",
+    options: ["Microsoft", "Sun Microsystems", "IBM", "Oracle"],
+    answer: 1
+  }
+];
