5482974c4cd7f56cb68c12e675e21f64be3cacfb
mahmoud adil <mahmod.adil2001@gmail.com>
2025-08-30 19:07:39 +0300

    Toggle visibility for operative lecture 2 version 1

--- show.js
+++ show.js
@@ -98,7 +98,9 @@ export const visibleLectures = {
             3,
             4
         ],
-        "2": [],
+        "2": [
+            1
+        ],
         "twentytwo": []
     },
     "oralpathology": {
