cd284c03cb38f5192faa60d4ae418c7bd9cf575b
mahmoud adil <mahmod.adil2001@gmail.com>
2025-08-30 19:30:56 +0300

    Toggle visibility for oralpathology lecture 2 version 1

--- show.js
+++ show.js
@@ -113,7 +113,9 @@ export const visibleLectures = {
             3,
             4
         ],
-        "2": [],
+        "2": [
+            1
+        ],
         "eight": []
     },
     "oralsurgery": {
