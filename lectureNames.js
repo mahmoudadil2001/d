808f15617c8d31f40d526e891dfcde7eb3b51a01
mahmoud adil <mahmod.adil2001@gmail.com>
2025-08-30 19:07:30 +0300

    Update lecture name for operative lecture 2

--- lectureNames.js
+++ lectureNames.js
@@ -97,7 +97,7 @@ export const lectureNames = {
     },
     "operative": {
         "1": "Assessment, Diagnosis, Treatment Planning",
-        "2": "Unknown",
+        "2": " Assessment, Diagnosis, Treatment Planning 2",
         "3": "Unknown",
         "4": "Unknown",
         "5": "Unknown",
