145d6f2c4758eab38caffb8786928979d37d8474
mahmoud adil <mahmod.adil2001@gmail.com>
2025-10-09 20:28:46 +0300

    Update content for oralsurgery3_v1.js

--- oralsurgery/oralsurgery3/oralsurgery3_v1.js
+++ oralsurgery/oralsurgery3/oralsurgery3_v1.js
@@ -1,7 +1,152 @@
 export const questions = [
   {
-    question: "Sample question for Oralsurgery Lecture 3, Version 1?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
-    answer: 0
+    question: "What is the most common cause of community-acquired native valve endocarditis?",
+    options: [
+      "Staphylococcus aureus",
+      "Streptococcus viridans",
+      "Candida species",
+      "HACEK group"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which cardiac condition is NOT among the highest risk for IE requiring antibiotic prophylaxis?",
+    options: [
+      "Prosthetic cardiac valve",
+      "Previous infective endocarditis",
+      "Mitral valve prolapse without regurgitation",
+      "Unrepaired cyanotic congenital heart disease"
+    ],
+    answer: 2
+  },
+  {
+    question: "What are Osler's nodes caused by?",
+    options: [
+      "Septic emboli",
+      "Immune-complex deposition",
+      "Direct bacterial invasion",
+      "Vitamin deficiency"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which diagnostic criterion for IE is considered a MAJOR criterion in the Duke criteria?",
+    options: [
+      "Fever",
+      "Predisposing heart condition",
+      "Positive echocardiography",
+      "Immunologic phenomena"
+    ],
+    answer: 2
+  },
+  {
+    question: "For which dental procedure is antibiotic prophylaxis for IE recommended?",
+    options: [
+      "Taking dental radiographs",
+      "Routine anesthetic injections through noninfected tissue",
+      "Manipulation of gingival tissues",
+      "Placement of removable prosthodontic appliances"
+    ],
+    answer: 2
+  },
+  {
+    question: "What is the recommended adult prophylactic dose of amoxicillin for IE prevention?",
+    options: [
+      "500 mg",
+      "1 g",
+      "2 g",
+      "3 g"
+    ],
+    answer: 2
+  },
+  {
+    question: "Which pathogen has emerged as the most common cause of IE in some recent studies?",
+    options: [
+      "Streptococcus viridans",
+      "Staphylococcus aureus",
+      "Candida albicans",
+      "Eikenella corrodens"
+    ],
+    answer: 1
+  },
+  {
+    question: "What is the primary mechanism of Janeway lesions?",
+    options: [
+      "Immune-complex deposition",
+      "Septic emboli forming micro-abscesses",
+      "Vitamin deficiency",
+      "Direct trauma"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which antibiotic should be used for IE prophylaxis in a penicillin-allergic patient who can take oral medication?",
+    options: [
+      "Amoxicillin",
+      "Ampicillin",
+      "Cephalexin",
+      "Clindamycin"
+    ],
+    answer: 3
+  },
+  {
+    question: "What is the most serious cardiac complication of rheumatic fever?",
+    options: [
+      "Pericarditis",
+      "Subendocardial inflammation leading to valvular damage",
+      "Myocardial infarction",
+      "Cardiac arrhythmias"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which congenital heart disease is classified as cyanotic?",
+    options: [
+      "Atrial septal defect",
+      "Ventricular septal defect",
+      "Fallot's tetralogy",
+      "Mitral valve prolapse"
+    ],
+    answer: 2
+  },
+  {
+    question: "For how long after prosthetic material placement in CHD repair is antibiotic prophylaxis recommended?",
+    options: [
+      "1 month",
+      "3 months",
+      "6 months",
+      "Lifelong"
+    ],
+    answer: 2
+  },
+  {
+    question: "Which oral manifestation is associated with congenital heart disease?",
+    options: [
+      "Lichen planus",
+      "Enamel hypoplasia",
+      "Oral candidiasis",
+      "Geographic tongue"
+    ],
+    answer: 1
+  },
+  {
+    question: "What is the recommended timing for antibiotic administration before dental procedures?",
+    options: [
+      "2-4 hours before",
+      "30-60 minutes before",
+      "Immediately after",
+      "24 hours before"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which of the following is a MINOR criterion in the Duke criteria for IE diagnosis?",
+    options: [
+      "Positive blood cultures",
+      "Evidence of endocardial involvement",
+      "Vascular phenomena",
+      "New valvular regurgitation"
+    ],
+    answer: 2
   }
 ];
