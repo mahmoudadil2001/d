4d1a3a9a99b6b86d34d3ed1838b64f2d6e38b313
mahmoud adil <mahmod.adil2001@gmail.com>
2025-10-14 16:38:18 +0300

    Update content for periodontology4_v3.js

--- periodontology/periodontology4/periodontology4_v3.js
+++ periodontology/periodontology4/periodontology4_v3.js
@@ -1,7 +1,77 @@
 export const questions = [
   {
-    question: "Sample question for Periodontology Lecture 4, Version 3?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
-    answer: 0
+    question: "The alveolar process is a structure that is:",
+    options: ["Independent of tooth presence", "Tooth-dependent", "Part of the nasal cavity", "Only found in the maxilla"],
+    answer: 1
+  },
+  {
+    question: "Which part of the alveolar process is also called 'bundle bone' due to embedded Sharpey's fibers?",
+    options: ["Cancellous Bone", "External Cortical Plate", "Alveolar Bone Proper", "Basal Bone"],
+    answer: 2
+  },
+  {
+    question: "The interdental septum is also known as:",
+    options: ["Inter radicular bone", "Interproximal bone", "Radicular bone", "Cribriform plate"],
+    answer: 1
+  },
+  {
+    question: "Volkmann's canals serve to connect the PDL with the:",
+    options: ["Gingiva", "Cancellous Bone", "Tooth Pulp", "Oral Mucosa"],
+    answer: 1
+  },
+  {
+    question: "Foci of red bone marrow in the adult jaw may appear radiographically as:",
+    options: ["Zones of Radiopacity", "Zones of Radiolucency", "Lamina Dura", "Bundle Bone"],
+    answer: 1
+  },
+  {
+    question: "Which defect is more common on anterior teeth and facial bone?",
+    options: ["Fenestration", "Dehiscence", "Cribriform Plate", "Haversian System"],
+    answer: 1
+  },
+  {
+    question: "Osteoblasts become trapped in the bone matrix and are then called:",
+    options: ["Osteoclasts", "Osteocytes", "Fibroblasts", "Cementoblasts"],
+    answer: 1
+  },
+  {
+    question: "Bone is composed of approximately what ratio of inorganic to organic matter?",
+    options: ["1/3 Inorganic, 2/3 Organic", "2/3 Inorganic, 1/3 Organic", "1/2 Inorganic, 1/2 Organic", "3/4 Inorganic, 1/4 Organic"],
+    answer: 1
+  },
+  {
+    question: "The concentric layers of bone matrix around a Haversian canal are called:",
+    options: ["Canaliculi", "Lamellae", "Lacunae", "Trabeculae"],
+    answer: 1
+  },
+  {
+    question: "Bone remodeling in response to occlusal forces is a process of:",
+    options: ["Resorption only", "Formation only", "Both resorption and formation", "Neither resorption nor formation"],
+    answer: 2
+  },
+  {
+    question: "Which of the following contains osteoblasts and covers the outer bone surface?",
+    options: ["Endosteum", "Periosteum", "Cementum", "PDL"],
+    answer: 1
+  },
+  {
+    question: "Nutrients and waste are exchanged between osteocytes via:",
+    options: ["Haversian Canals", "Volkmann's Canals", "Canaliculi", "The Periodontal Ligament"],
+    answer: 2
+  },
+  {
+    question: "The degradation of the organic bone matrix during resorption is performed by:",
+    options: ["Acidic Environment", "Minerals", "Enzymes like cathepsin", "Osteoblast activity"],
+    answer: 2
+  },
+  {
+    question: "The space in the alveolar bone that accommodates a tooth root is the:",
+    options: ["Alveolar Process", "Alveolus", "Basal Bone", "Cribriform Plate"],
+    answer: 1
+  },
+  {
+    question: "Which factor is a SYSTEMIC regulator of alveolar bone remodeling?",
+    options: ["Occlusal Forces", "Tooth Position", "Parathyroid Hormone", "Mesial Migration"],
+    answer: 2
   }
 ];
