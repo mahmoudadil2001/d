fd20fd2894dcade16dd97890e47962c2bbc1d257
mahmoud adil <mahmod.adil2001@gmail.com>
2025-10-27 14:30:36 +0300

    Update content for periodontology6_v3.js

--- periodontology/periodontology6/periodontology6_v3.js
+++ periodontology/periodontology6/periodontology6_v3.js
@@ -1,7 +1,77 @@
 export const questions = [
   {
-    question: "Sample question for Periodontology Lecture 6, Version 3?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    question: "Periodontitis is characterized by all EXCEPT:",
+    options: ["Progressive destruction of tooth-supporting apparatus", "Presence of periodontal pockets", "Gingival bleeding", "Always symptomatic"],
+    answer: 3
+  },
+  {
+    question: "Stage IV periodontitis involves bone loss of:",
+    options: ["<15%", "15-33%", "33-66%", ">66%"],
+    answer: 3
+  },
+  {
+    question: "The extent of periodontitis is 'localized' when involved sites are:",
+    options: ["<30%", "≥30%", "≥50%", "≥70%"],
     answer: 0
+  },
+  {
+    question: "Stable periodontitis is characterized by BOP of:",
+    options: ["<10%", "10-20%", "20-30%", ">30%"],
+    answer: 0
+  },
+  {
+    question: "Which is a major risk factor for periodontitis?",
+    options: ["Smoking", "Obesity", "Diabetes mellitus", "All of the above"],
+    answer: 3
+  },
+  {
+    question: "Necrotizing ulcerative gingivitis is predisposed by:",
+    options: ["Emotional stress", "HIV infection", "Poor diet", "All of the above"],
+    answer: 3
+  },
+  {
+    question: "A periodontal abscess in a periodontitis patient can be classified as:",
+    options: ["Acute exacerbation", "After scaling", "After surgery", "All of the above"],
+    answer: 3
+  },
+  {
+    question: "Endo-periodontal lesions may present with:",
+    options: ["Deep pockets to apex", "Negative pulp vitality", "Sinus tract", "All of the above"],
+    answer: 3
+  },
+  {
+    question: "Which is a mucogingival deformity?",
+    options: ["Gingival excess", "Abnormal color", "Lack of keratinized gingiva", "All of the above"],
+    answer: 3
+  },
+  {
+    question: "Stage I periodontitis shows radiographic bone loss of:",
+    options: ["<15%", "15-33%", "33-66%", ">66%"],
+    answer: 0
+  },
+  {
+    question: "Grade C periodontitis includes a bone loss/age ratio of:",
+    options: ["<0.5", "0.5-1", ">1", ">2"],
+    answer: 2
+  },
+  {
+    question: "Which condition is listed under 'Systemic disorders that result in loss of periodontal tissues independent of periodontitis'?",
+    options: ["Neoplasms", "Diabetes", "Obesity", "Smoking"],
+    answer: 0
+  },
+  {
+    question: "Necrotizing periodontitis includes all EXCEPT:",
+    options: ["Necrotizing ulcerative gingivitis", "Necrotizing ulcerative periodontitis", "Necrotizing stomatitis", "Necrotizing pulpitis"],
+    answer: 3
+  },
+  {
+    question: "A periodontal abscess can be caused by:",
+    options: ["Foreign bodies", "Cemental tears", "Orthodontic forces", "All of the above"],
+    answer: 3
+  },
+  {
+    question: "The biologic width used in bone loss estimation is:",
+    options: ["1 mm", "2 mm", "3 mm", "4 mm"],
+    answer: 1
   }
 ];
