195cb6954d9d84714deb40a73d132b5fa2e720cd
mahmoud adil <mahmod.adil2001@gmail.com>
2025-11-14 11:33:43 +0300

    Update content for pedodontics27_v3.js

--- pedodontics/pedodontics27/pedodontics27_v3.js
+++ pedodontics/pedodontics27/pedodontics27_v3.js
@@ -1,7 +1,77 @@
 export const questions = [
   {
-    question: "Sample question for Pedodontics Lecture 27, Version 3?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    question: "Which factor is considered a general factor in malocclusion?",
+    options: ["Tooth loss", "Crowding", "Skeletal factors", "Impacted teeth"],
+    answer: 2
+  },
+  {
+    question: "Skeletal Class III malocclusion involves:",
+    options: ["Lower jaw forward", "Lower jaw backward", "Ideal jaw relationship", "Vertical excess"],
     answer: 0
+  },
+  {
+    question: "Lip competence refers to:",
+    options: ["Lips meeting at rest", "Lips not meeting at rest", "Lips sealed only with effort", "Lips apart during speech"],
+    answer: 0
+  },
+  {
+    question: "The sagittal relationship of jaws is assessed in relation to:",
+    options: ["The cervical spine", "The cranial base", "The hyoid bone", "The nasal bone"],
+    answer: 1
+  },
+  {
+    question: "Scissor bite occurs when:",
+    options: ["Lower jaw is wider", "Upper jaw is wider", "Jaws are equal", "Vertical dimension is increased"],
+    answer: 1
+  },
+  {
+    question: "Alveolar bone is supported by:",
+    options: ["Basal bone", "Muscles", "Teeth", "Periodontal ligament"],
+    answer: 0
+  },
+  {
+    question: "In skeletal Class II, the lower lip may cause retroclination of:",
+    options: ["Upper incisors", "Lower incisors", "Molars", "Canines"],
+    answer: 1
+  },
+  {
+    question: "The ideal lip line is at the:",
+    options: ["Gingival margin of upper incisors", "Incisal edge of upper incisors", "Center of upper incisor crowns", "Root apex of upper incisors"],
+    answer: 2
+  },
+  {
+    question: "Which of the following is NOT a skeletal factor?",
+    options: ["Jaw size", "Jaw shape", "Jaw position", "Tongue size"],
+    answer: 3
+  },
+  {
+    question: "Muscles affecting tooth position originate mainly from:",
+    options: ["Alveolar bone", "Basal bone", "Teeth", "Soft palate"],
+    answer: 1
+  },
+  {
+    question: "A low lip line may cause:",
+    options: ["Retroclination of upper incisors", "Proclination of upper incisors", "No effect", "Molar extrusion"],
+    answer: 1
+  },
+  {
+    question: "Which skeletal class is associated with ideally related jaws?",
+    options: ["Class I", "Class II", "Class III", "Class IV"],
+    answer: 0
+  },
+  {
+    question: "Lip incompetence is often due to:",
+    options: ["Long upper lip", "Short upper lip", "Normal lip length", "Tongue size"],
+    answer: 1
+  },
+  {
+    question: "The vertical dimension of the face is influenced by the:",
+    options: ["Genial angle", "Sagittal suture", "Nasal aperture", "Zygomatic arch"],
+    answer: 0
+  },
+  {
+    question: "In skeletal Class II division 2, the upper incisors are typically:",
+    options: ["Proclined", "Retroclined", "Extruded", "Intruded"],
+    answer: 1
   }
 ];
