c5fd02308e5cd67a3b533834600bad2a85577e4b
mahmoud adil <mahmod.adil2001@gmail.com>
2025-11-14 17:43:11 +0300

    Update content for pedodontics29_v4.js

--- pedodontics/pedodontics29/pedodontics29_v4.js
+++ pedodontics/pedodontics29/pedodontics29_v4.js
@@ -1,7 +1,77 @@
 export const questions = [
   {
-    question: "Sample question for Pedodontics Lecture 29, Version 4?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
-    answer: 0
+    question: "Anomalies of tooth shape include all EXCEPT:",
+    options: ["Dilaceration", "True fusion", "Gemination", "Blanch test"],
+    answer: 3
+  },
+  {
+    question: "Which anomaly may complicate tooth extraction?",
+    options: ["Gemination", "Dilaceration", "Talon cusp", "Midline diastema"],
+    answer: 1
+  },
+  {
+    question: "Gemination results in:",
+    options: ["Two complete teeth", "One large tooth", "Two incomplete teeth", "Fused roots"],
+    answer: 2
+  },
+  {
+    question: "The talon cusp is found on:",
+    options: ["Molars", "Incisors", "Premolars", "Deciduous teeth only"],
+    answer: 1
+  },
+  {
+    question: "Grinding a talon cusp often leads to:",
+    options: ["Concrescence", "Pulpal exposure", "Dilaceration", "Fusion"],
+    answer: 1
+  },
+  {
+    question: "Midline diastema can be caused by:",
+    options: ["Talon cusp", "Dens in dente", "Abnormal frenum", "Dilaceration"],
+    answer: 2
+  },
+  {
+    question: "The blanch test is positive if:",
+    options: ["Tooth is mobile", "Bleeding occurs", "Blanching is seen interdentally", "Pain is elicited"],
+    answer: 2
+  },
+  {
+    question: "Premature loss of a deciduous tooth affects occlusion if:",
+    options: ["Permanent tooth is erupting", "Space is maintained", "Succedaneous tooth is not near eruption", "Arch length is increased"],
+    answer: 2
+  },
+  {
+    question: "Prolonged retention of deciduous teeth may cause:",
+    options: ["Dens in dente", "Impaction of permanent teeth", "Talon cusp", "Concrescence"],
+    answer: 1
+  },
+  {
+    question: "The most commonly impacted anterior tooth is the:",
+    options: ["Maxillary central incisor", "Maxillary canine", "Mandibular lateral incisor", "Mandibular canine"],
+    answer: 1
+  },
+  {
+    question: "Maxillary canine impaction is due to:",
+    options: ["Short eruption path", "Early root completion", "Long eruption path", "Small tooth size"],
+    answer: 2
+  },
+  {
+    question: "Concrescence involves:",
+    options: ["Enamel fusion", "Dentin fusion", "Cementum fusion", "Pulp fusion"],
+    answer: 2
+  },
+  {
+    question: "True fusion arises from:",
+    options: ["Single tooth germ", "Two tooth germs", "Cementum only", "Enamel invagination"],
+    answer: 1
+  },
+  {
+    question: "Mesial eruption of first molars after deciduous loss can cause:",
+    options: ["Arch length increase", "Arch length decrease", "No change", "Talon cusp"],
+    answer: 1
+  },
+  {
+    question: "An IOPA radiograph with frenal involvement may show:",
+    options: ["Dilaceration", "Interdental notching", "Gemination", "Concrescence"],
+    answer: 1
   }
 ];
