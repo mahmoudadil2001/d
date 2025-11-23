a7da7b595cba310834555217a5711f1a096ede7d
mahmoud adil <mahmod.adil2001@gmail.com>
2025-11-23 19:17:11 +0300

    Update content for generalsurgery7_v3.js

--- generalsurgery/generalsurgery7/generalsurgery7_v3.js
+++ generalsurgery/generalsurgery7/generalsurgery7_v3.js
@@ -1,7 +1,77 @@
 export const questions = [
   {
-    question: "Sample question for Generalsurgery Lecture 7, Version 3?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    question: "Daily exogenous fluid intake is roughly:",
+    options: ["1-2 liters", "2-3 liters", "3-4 liters", "4-5 liters"],
+    answer: 1
+  },
+  {
+    question: "Endogenous water comes from:",
+    options: ["Drinking", "Food", "Oxidation", "IV fluids"],
+    answer: 2
+  },
+  {
+    question: "Water loss via bowel motion is about:",
+    options: ["60-150 ml", "200-300 ml", "400-500 ml", "500-600 ml"],
     answer: 0
+  },
+  {
+    question: "A sign of water depletion is:",
+    options: ["Polyuria", "Weakness", "Edema", "Puffy face"],
+    answer: 1
+  },
+  {
+    question: "Water intoxication may cause:",
+    options: ["Dry skin", "Nausea and vomiting", "Hypertension", "Sunken eyes"],
+    answer: 1
+  },
+  {
+    question: "Sodium depletion can be caused by:",
+    options: ["Excessive IV fluids", "Adrenocortical insufficiency", "Hypernatremia", "Alkalosis"],
+    answer: 1
+  },
+  {
+    question: "Hypernatremia can result from:",
+    options: ["Diarrhea", "Excessive IV saline", "Vomiting", "Fistula"],
+    answer: 1
+  },
+  {
+    question: "A clinical feature of hypernatremia is:",
+    options: ["Sunken eyes", "Dry tongue", "Puffy face", "Hypotension"],
+    answer: 2
+  },
+  {
+    question: "Hypokalemia can be caused by:",
+    options: ["Trauma", "Alkalosis", "Acidosis", "Hyperkalemia"],
+    answer: 0
+  },
+  {
+    question: "A symptom of hypokalemia is:",
+    options: ["Tachycardia", "Slow speech", "Hypertension", "Tetany"],
+    answer: 1
+  },
+  {
+    question: "The blood pH is normally:",
+    options: ["7.0-7.2", "7.2-7.4", "7.36-7.44", "7.5-7.6"],
+    answer: 2
+  },
+  {
+    question: "Acid-base balance is maintained by:",
+    options: ["Sodium balance", "Bicarbonate system", "Potassium balance", "Chloride shift"],
+    answer: 1
+  },
+  {
+    question: "Alkalosis can be due to:",
+    options: ["Acid excess", "Base deficit", "Base excess", "CO2 retention"],
+    answer: 2
+  },
+  {
+    question: "A clinical feature of alkalosis is:",
+    options: ["Tachypnea", "Tetany", "Hypertension", "Bradycardia"],
+    answer: 1
+  },
+  {
+    question: "Metabolic acidosis may present with:",
+    options: ["Apnea", "Tetany", "Tachycardia", "Hysteria"],
+    answer: 2
   }
 ];
