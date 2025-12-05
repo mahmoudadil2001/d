6128157e0d5ab37f4052183028cfb16cee4d6572
mahmoud adil <mahmod.adil2001@gmail.com>
2025-12-06 01:48:56 +0300

    Update content for generalmedicine8_v1.js

--- generalmedicine/generalmedicine8/generalmedicine8_v1.js
+++ generalmedicine/generalmedicine8/generalmedicine8_v1.js
@@ -1,7 +1,102 @@
 export const questions = [
   {
-    question: "Sample question for Generalmedicine Lecture 8, Version 1?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
-    answer: 0
+    question: "Which GI disease is most commonly seen in outpatient clinics?",
+    options: ["IBD", "Pseudomembranous colitis", "GERD", "Peptic ulcer disease"],
+    answer: 2
+  },
+  {
+    question: "The main function of the esophagus is to:",
+    options: ["Digest food", "Absorb nutrients", "Conduct food to the stomach", "Store food"],
+    answer: 2
+  },
+  {
+    question: "Peristalsis refers to:",
+    options: [
+      "Passive diffusion of food",
+      "Wavelike muscular contractions",
+      "Hormonal secretion",
+      "Mechanical digestion"
+    ],
+    answer: 1
+  },
+  {
+    question: "The lower esophageal sphincter is considered:",
+    options: [
+      "Anatomic only",
+      "Physiologic only",
+      "Both anatomic and physiologic",
+      "Absent completely"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which hormone increases tone of the gastroesophageal sphincter?",
+    options: ["Secretin", "Glucagon", "Cholecystokinin", "Gastrin"],
+    answer: 3
+  },
+  {
+    question: "GERD mainly causes reflux of:",
+    options: ["Bile", "Food particles", "Acidic gastric contents", "Pancreatic enzymes"],
+    answer: 2
+  },
+  {
+    question: "Prolonged GERD may lead to:",
+    options: ["Squamous hyperplasia", "Adenocarcinoma", "Fibrosarcoma", "Muscular dystrophy"],
+    answer: 1
+  },
+  {
+    question: "The adult stomach has an approximate capacity of:",
+    options: ["500 mL", "1000 mL", "1500 mL", "2000 mL"],
+    answer: 2
+  },
+  {
+    question: "Chyme is best described as:",
+    options: [
+      "Alkaline fluid in the mouth",
+      "Acidic semifluid in the stomach",
+      "Pure gastric acid",
+      "Undigested food in intestine"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which part of the stomach is dome-shaped?",
+    options: ["Body", "Fundus", "Pylorus", "Antrum"],
+    answer: 1
+  },
+  {
+    question: "The pyloric sphincter controls the flow into the:",
+    options: ["Jejunum", "Ileum", "Duodenum", "Colon"],
+    answer: 2
+  },
+  {
+    question: "Gastric rugae mainly flatten when the stomach is:",
+    options: ["Contracted", "Empty", "Distended", "Inflamed"],
+    answer: 2
+  },
+  {
+    question: "Gastric pain is mainly caused by:",
+    options: [
+      "Mucosal erosion",
+      "Muscle stretching or spasm",
+      "Acid secretion",
+      "Bacterial invasion"
+    ],
+    answer: 1
+  },
+  {
+    question: "Total gastrectomy is performed mainly for:",
+    options: ["GERD", "PUD", "Stomach cancer", "IBD"],
+    answer: 2
+  },
+  {
+    question: "Nasogastric intubation is used to:",
+    options: [
+      "Visualize stomach lining",
+      "Empty or decompress the stomach",
+      "Remove tumors",
+      "Measure oxygen levels"
+    ],
+    answer: 1
   }
 ];
