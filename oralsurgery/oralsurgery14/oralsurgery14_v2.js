3c10455eed47bb8c349afeda0d6de8aeb8115b9b
mahmoud adil <mahmod.adil2001@gmail.com>
2025-12-10 15:35:35 +0300

    Update content for oralsurgery14_v2.js

--- oralsurgery/oralsurgery14/oralsurgery14_v2.js
+++ oralsurgery/oralsurgery14/oralsurgery14_v2.js
@@ -1,7 +1,77 @@
 export const questions = [
   {
-    question: "Sample question for Oralsurgery Lecture 14, Version 2?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    question: "Which condition is characterized by 'blue bloaters'?",
+    options: ["Emphysema", "Chronic bronchitis", "Asthma", "Tuberculosis"],
+    answer: 1
+  },
+  {
+    question: "What pathological change is seen in chronic bronchitis?",
+    options: ["Alveolar destruction", "Increased mucous gland size", "Pleural thickening", "Vascular dilation"],
+    answer: 1
+  },
+  {
+    question: "Which genetic factor is associated with emphysema?",
+    options: ["Absence of alpha1-antitrypsin", "BRCA mutation", "CFTR gene defect", "HLA-B27"],
     answer: 0
+  },
+  {
+    question: "In emphysema, patients often exhale with:",
+    options: ["Rapid shallow breaths", "Pursed lips", "Wheezing only", "No change"],
+    answer: 1
+  },
+  {
+    question: "Which measurement is reduced in arterial blood gas in chronic bronchitis?",
+    options: ["PCO2", "PO2", "pH", "Bicarbonate"],
+    answer: 1
+  },
+  {
+    question: "Which class of bronchodilators includes salbutamol?",
+    options: ["Anticholinergics", "Beta2-agonists", "Corticosteroids", "Phosphodiesterase inhibitors"],
+    answer: 1
+  },
+  {
+    question: "In COPD patients, when should oxygen saturation be supplemented with low-flow oxygen?",
+    options: ["Below 99%", "Below 95%", "Below 90%", "Below 85%"],
+    answer: 1
+  },
+  {
+    question: "Which nerve block should be avoided in COPD patients?",
+    options: ["Inferior alveolar bilateral", "Infraorbital", "Mental", "Long buccal"],
+    answer: 0
+  },
+  {
+    question: "Which asthma type is triggered by emotional stress or acid reflux?",
+    options: ["Extrinsic", "Intrinsic", "Exercise-induced", "Allergic"],
+    answer: 1
+  },
+  {
+    question: "What is a common oral manifestation in asthmatic patients using beta-agonists?",
+    options: ["Increased salivary flow", "Decreased salivary flow", "Oral candidiasis", "Leukoplakia"],
+    answer: 1
+  },
+  {
+    question: "Which drug used in asthma may cause prolonged INR and bleeding tendency?",
+    options: ["Beta-agonists", "Leukotriene modifiers", "Theophylline", "Corticosteroids"],
+    answer: 1
+  },
+  {
+    question: "During an asthma attack, what is the recommended first medication?",
+    options: ["Oral corticosteroid", "Fast-acting bronchodilator", "Subcutaneous epinephrine", "Antihistamine"],
+    answer: 1
+  },
+  {
+    question: "What organism causes tuberculosis?",
+    options: ["Streptococcus pneumoniae", "Mycobacterium tuberculosis", "Haemophilus influenzae", "Klebsiella pneumoniae"],
+    answer: 1
+  },
+  {
+    question: "Which TB test is not affected by prior BCG vaccination?",
+    options: ["Tuberculin skin test", "Interferon gamma release assay", "Chest X-ray", "Sputum smear"],
+    answer: 1
+  },
+  {
+    question: "In dental management, when should treatment for active TB patients be performed?",
+    options: ["Routine elective care", "Only urgent care", "Any care with mask", "No restrictions"],
+    answer: 1
   }
 ];
