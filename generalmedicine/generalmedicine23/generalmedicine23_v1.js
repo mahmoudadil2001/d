4a8aaeed790081e19308369e551159b5aa92a21d
mahmoud adil <mahmod.adil2001@gmail.com>
2026-01-04 14:49:31 +0300

    Update content for generalmedicine23_v1.js

--- generalmedicine/generalmedicine23/generalmedicine23_v1.js
+++ generalmedicine/generalmedicine23/generalmedicine23_v1.js
@@ -1,7 +1,78 @@
+// Version 1
 export const questions = [
   {
-    question: "Sample question for Generalmedicine Lecture 23, Version 1?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    question: "During general appearance assessment in CVS exam, which sign suggests distress?",
+    options: ["Visible pulsations", "Breathlessness", "Scar presence", "Normal weight"],
+    answer: 1
+  },
+  {
+    question: "Which syndrome is associated with cardiovascular disease?",
+    options: ["Marfan's syndrome", "Down's syndrome", "Turner's syndrome", "All of the above"],
+    answer: 3
+  },
+  {
+    question: "Capillary refill time is assessed in which part of the examination?",
+    options: ["Hands", "Face", "Precordium", "Lower limbs"],
     answer: 0
+  },
+  {
+    question: "Splinter hemorrhages are peripheral signs of:",
+    options: ["Hyperlipidemia", "Infective endocarditis", "Mitral stenosis", "Aortic regurgitation"],
+    answer: 1
+  },
+  {
+    question: "Malar rash over the upper cheeks is seen in:",
+    options: ["Severe mitral stenosis", "Aortic stenosis", "Pulmonary embolism", "Hypertrophic cardiomyopathy"],
+    answer: 0
+  },
+  {
+    question: "Which chest deformity is characterized by a sunken sternum?",
+    options: ["Pectus carinatum", "Pectus excavatum", "Barrel chest", "Kyphosis"],
+    answer: 1
+  },
+  {
+    question: "The apex beat is normally palpated in which intercostal space?",
+    options: ["2nd ICS, midclavicular line", "4th ICS, midclavicular line", "5th ICS, midclavicular line", "6th ICS, midclavicular line"],
+    answer: 2
+  },
+  {
+    question: "A 'tapping' apex beat is characteristic of:",
+    options: ["Mitral stenosis", "Aortic regurgitation", "Ventricular septal defect", "Pericardial effusion"],
+    answer: 0
+  },
+  {
+    question: "A double apical impulse is typical of:",
+    options: ["Hypertrophic cardiomyopathy", "Dilated cardiomyopathy", "Aortic stenosis", "Pulmonary hypertension"],
+    answer: 0
+  },
+  {
+    question: "To feel a right ventricular heave, the examiner should apply pressure in which area?",
+    options: ["Apex", "Left parasternal area", "Upper right sternal border", "Epigastric area"],
+    answer: 1
+  },
+  {
+    question: "A thrill of aortic stenosis is usually best felt over:",
+    options: ["Apex", "Lower left sternal border", "Upper right sternal border", "Carotid artery"],
+    answer: 2
+  },
+  {
+    question: "Which heart sound is best heard with the stethoscope bell?",
+    options: ["S1", "S2", "S3", "All of the above"],
+    answer: 2
+  },
+  {
+    question: "The first heart sound (S1) corresponds to:",
+    options: ["Aortic valve closure", "Pulmonic valve closure", "Mitral and tricuspid valve closure", "Ventricular filling"],
+    answer: 2
+  },
+  {
+    question: "A pericardial rub is best heard with the:",
+    options: ["Bell at apex", "Diaphragm at left sternal border", "Bell at carotid", "Diaphragm at lung bases"],
+    answer: 1
+  },
+  {
+    question: "Which murmur is typically systolic?",
+    options: ["Mitral stenosis", "Aortic regurgitation", "Aortic stenosis", "Mitral regurgitation"],
+    answer: 3
   }
 ];
