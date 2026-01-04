8c21a6a1c6d7c0332a7cf27e996a307e3d122df5
mahmoud adil <mahmod.adil2001@gmail.com>
2026-01-04 14:49:38 +0300

    Update content for generalmedicine23_v2.js

--- generalmedicine/generalmedicine23/generalmedicine23_v2.js
+++ generalmedicine/generalmedicine23/generalmedicine23_v2.js
@@ -1,7 +1,78 @@
+// Version 2
 export const questions = [
   {
-    question: "Sample question for Generalmedicine Lecture 23, Version 2?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    question: "What should be examined at the end of the general appearance assessment?",
+    options: ["Only pulse", "Skin for petechiae, temperature, urinalysis", "Only blood pressure", "Only JVP"],
+    answer: 1
+  },
+  {
+    question: "Tobacco staining on fingers suggests:",
+    options: ["Hyperlipidemia", "Smoking history", "Infective endocarditis", "Cyanotic heart disease"],
+    answer: 1
+  },
+  {
+    question: "Osler's nodes are found on:",
+    options: ["Palmar aspects", "Soles of feet", "Over cheeks", "On the back"],
     answer: 0
+  },
+  {
+    question: "Bilateral cyanotic or dusky pink discoloration over the upper cheeks is due to:",
+    options: ["Capillary refill delay", "Arteriovenous anastomosis & vascular stasis", "Hyperlipidemia", "Pulmonary embolism"],
+    answer: 1
+  },
+  {
+    question: "Which scar is typical of previous cardiac surgery?",
+    options: ["Appendectomy scar", "Midline sternotomy", "Knee surgery scar", "Laparoscopic scar"],
+    answer: 1
+  },
+  {
+    question: "The apex beat may be impalpable in which condition?",
+    options: ["Asthma or emphysema", "Left ventricular hypertrophy", "Mitral stenosis", "Aortic regurgitation"],
+    answer: 0
+  },
+  {
+    question: "In left ventricular dilatation, the apex beat is displaced:",
+    options: ["Superiorly and medially", "Inferiorly and laterally", "Only superiorly", "Only medially"],
+    answer: 1
+  },
+  {
+    question: "Dextrocardia has a prevalence of approximately:",
+    options: ["1:100", "1:1,000", "1:10,000", "1:100,000"],
+    answer: 2
+  },
+  {
+    question: "To palpate a right ventricular heave, the patient should hold their breath in:",
+    options: ["Inspiration", "Expiration", "Mid-breath", "It doesn't matter"],
+    answer: 1
+  },
+  {
+    question: "Thrills are best felt using:",
+    options: ["Heel of the hand", "Flat of the fingers", "Fingertips only", "Palm only"],
+    answer: 1
+  },
+  {
+    question: "Which area is NOT a primary auscultation site for heart sounds?",
+    options: ["Apex", "Lower left sternal border", "Upper right sternal border", "Right mid-axillary line"],
+    answer: 3
+  },
+  {
+    question: "The second heart sound (S2) corresponds to:",
+    options: ["Mitral valve closure", "Aortic and pulmonic valve closure", "Tricuspid valve opening", "Ventricular contraction"],
+    answer: 1
+  },
+  {
+    question: "An S4 heart sound is associated with:",
+    options: ["Atrial contraction", "Rapid ventricular filling", "Aortic stenosis", "Mitral regurgitation"],
+    answer: 0
+  },
+  {
+    question: "A 'snap' sound is often heard in:",
+    options: ["Mitral stenosis", "Aortic regurgitation", "Pulmonary stenosis", "Tricuspid regurgitation"],
+    answer: 0
+  },
+  {
+    question: "Murmurs are described by all EXCEPT:",
+    options: ["Timing", "Duration", "Blood group", "Intensity"],
+    answer: 2
   }
 ];
