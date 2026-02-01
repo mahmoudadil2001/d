92024b8a09179cddeae7694c07dca42096884a96
mahmoud adil <mahmod.adil2001@gmail.com>
2026-02-01 18:58:16 +0300

    Update content for generalmedicine13_v2.js

--- generalmedicine/generalmedicine13/generalmedicine13_v2.js
+++ generalmedicine/generalmedicine13/generalmedicine13_v2.js
@@ -1,7 +1,77 @@
 export const questions = [
   {
-    question: "Sample question for Generalmedicine Lecture 13, Version 2?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    question: "Peptic ulcer disease results from chronic secretions of:",
+    options: ["Acid or pepsin", "Bile", "Insulin", "Mucus"],
     answer: 0
+  },
+  {
+    question: "In which region are gastric ulcers more frequent?",
+    options: ["Western populations", "Asia", "Africa", "South America"],
+    answer: 1
+  },
+  {
+    question: "First-degree relatives of PUD patients have how much greater risk?",
+    options: ["Equal risk", "Twofold", "Threefold", "Tenfold"],
+    answer: 2
+  },
+  {
+    question: "Long-term use of NSAIDs for more than ___ increases ulcer risk.",
+    options: ["1 week", "1 month", "6 months", "1 year"],
+    answer: 1
+  },
+  {
+    question: "Which bacterium is noninvasive but adheres to gastric mucosa?",
+    options: ["E. coli", "H. pylori", "C. difficile", "S. aureus"],
+    answer: 1
+  },
+  {
+    question: "H. pylori can persist in the stomach:",
+    options: ["For a few days", "For weeks", "Indefinitely", "Only during infection"],
+    answer: 2
+  },
+  {
+    question: "NSAID-induced ulcers are more common in the:",
+    options: ["Duodenum", "Stomach", "Esophagus", "Jejunum"],
+    answer: 1
+  },
+  {
+    question: "Which drug class is associated with esophageal and gastric ulcers?",
+    options: ["Bisphosphonates", "Statins", "Beta-blockers", "Diuretics"],
+    answer: 0
+  },
+  {
+    question: "Nervous control of gastric secretion is mediated by:",
+    options: ["Sympathetic nerves", "Vagus nerves", "Spinal nerves", "Cranial nerves"],
+    answer: 1
+  },
+  {
+    question: "Which substance stimulates parietal cell secretion of hydrochloric acid?",
+    options: ["Insulin", "Caffeine", "Alcohol", "Bile"],
+    answer: 1
+  },
+  {
+    question: "Tobacco smoke reduces levels of which protective molecule?",
+    options: ["Nitric oxide", "Oxygen", "Carbon dioxide", "Hydrogen"],
+    answer: 0
+  },
+  {
+    question: "Perforation of an ulcer leads to:",
+    options: ["Peritonitis", "Hypertension", "Diabetes", "Asthma"],
+    answer: 0
+  },
+  {
+    question: "Untreated ulcers may heal by fibrosis, causing:",
+    options: ["Gastric outlet obstruction", "Increased acid production", "Weight gain", "Improved digestion"],
+    answer: 0
+  },
+  {
+    question: "Which ulcer location has greater malignant potential?",
+    options: ["Duodenum", "Greater curvature of stomach", "Pylorus", "Cardia"],
+    answer: 1
+  },
+  {
+    question: "Melena refers to:",
+    options: ["Vomiting blood", "Black tarry stools", "Yellow stools", "Watery diarrhea"],
+    answer: 1
   }
 ];
