3cb21ee3222db64200b2eae5ad1019da5d4c2047
mahmoud adil <mahmod.adil2001@gmail.com>
2026-02-12 18:31:16 +0300

    Update lecture name for endodontics lecture 5

--- lectureNames.js
+++ lectureNames.js
@@ -4,7 +4,7 @@ export const lectureNames = {
         "2": "Classification Of Periapical Disease",
         "3": "Access Opening Preparation",
         "4": "Working length Determination",
-        "5": "root canal cleaning and shaping",
+        "5": "root canal cleaning and shaping🟦🟩",
         "6": "Clean& Shape Root Canal",
         "7": "Clean& Shape Root Canal 2",
         "8": "Root Canal Obturation",
