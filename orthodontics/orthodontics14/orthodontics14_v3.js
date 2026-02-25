f3e7b489f46103d0c314a04edad6f40cf59c7b42
mahmoud adil <mahmod.adil2001@gmail.com>
2026-02-25 19:51:45 +0300

    Update content for orthodontics14_v3.js

--- orthodontics/orthodontics14/orthodontics14_v3.js
+++ orthodontics/orthodontics14/orthodontics14_v3.js
@@ -1,7 +1,77 @@
 export const questions = [
   {
-    question: "Sample question for Orthodontics Lecture 14, Version 3?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
-    answer: 0
+    question: "Alveolar bone offers resistance to tooth movement up to:",
+    options: ["Any amount of force", "A certain amount of force, exceeding which causes bone remodeling", "No force at all", "Only light forces"],
+    answer: 1
+  },
+  {
+    question: "Which appliance uses the hard palate for anchorage?",
+    options: ["Lip bumper", "Trans-palatal arch", "Nance palatal button", "Headgear"],
+    answer: 2
+  },
+  {
+    question: "Hypotonic musculature can cause:",
+    options: ["Lingual movement of teeth", "Flaring of teeth", "No effect on teeth", "Tooth rotation only"],
+    answer: 1
+  },
+  {
+    question: "Hypertonic musculature can force teeth in which direction?",
+    options: ["Mesial direction", "Distal direction", "Lingual direction", "No movement"],
+    answer: 2
+  },
+  {
+    question: "Cervical headgears derive anchorage from:",
+    options: ["Forehead", "Back of neck or cervical region", "Chin", "Top of head"],
+    answer: 1
+  },
+  {
+    question: "Occipital and parietal headgears are used to:",
+    options: ["Protract maxilla", "Resist maxillary growth or move teeth distally", "Advance mandible", "Expand palate"],
+    answer: 1
+  },
+  {
+    question: "Face mask (reverse headgear) uses anchorage from:",
+    options: ["Back of neck", "Forehead and chin regions", "Top of head", "Shoulders"],
+    answer: 1
+  },
+  {
+    question: "Face mask therapy is used to:",
+    options: ["Retract maxilla", "Protract the maxilla", "Advance mandible", "Retract mandible"],
+    answer: 1
+  },
+  {
+    question: "Single or primary anchorage is defined as resistance by:",
+    options: ["Multiple teeth", "A single tooth with greater alveolar support", "Extra-oral structures", "Soft tissues"],
+    answer: 1
+  },
+  {
+    question: "Compound anchorage uses:",
+    options: ["One tooth only", "More than one tooth with greater anchorage potential", "Extra-oral structures only", "No teeth"],
+    answer: 1
+  },
+  {
+    question: "Reinforced anchorage utilizes:",
+    options: ["One type of resistance unit", "More than one type of resistance units", "No resistance units", "Only teeth"],
+    answer: 1
+  },
+  {
+    question: "Trans-palatal arch is used to:",
+    options: ["Move teeth forward", "Reinforce anchorage in fixed mechano-therapy", "Extract teeth", "Expand palate only"],
+    answer: 1
+  },
+  {
+    question: "Greater number of teeth being moved means:",
+    options: ["Less anchorage demand", "Same anchorage demand", "Greater anchorage demand", "No anchorage needed"],
+    answer: 2
+  },
+  {
+    question: "Movement of canines is tougher than incisors because canines have:",
+    options: ["Shorter roots", "Longer, larger and flatter root", "Multiple roots", "Smaller roots"],
+    answer: 1
+  },
+  {
+    question: "Molars are more difficult to move than premolars because they have:",
+    options: ["Shorter roots", "Single roots", "Greater number of roots", "Smaller size"],
+    answer: 2
   }
 ];
