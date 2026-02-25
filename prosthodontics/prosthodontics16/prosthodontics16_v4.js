d50af8e6493151e829dce437effbba6bf2f61e53
mahmoud adil <mahmod.adil2001@gmail.com>
2026-02-25 20:13:58 +0300

    Update content for prosthodontics16_v4.js

--- prosthodontics/prosthodontics16/prosthodontics16_v4.js
+++ prosthodontics/prosthodontics16/prosthodontics16_v4.js
@@ -1,7 +1,77 @@
 export const questions = [
   {
-    question: "Sample question for Prosthodontics Lecture 16, Version 4?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
-    answer: 0
+    question: "What is the definition of 'vertical dimension' in prosthodontics?",
+    options: ["The length of the maxillary arch", "The distance between two selected points, one on a fixed and one on a movable member", "The height of the occlusal rim", "The amount of space available for the tongue"],
+    answer: 1
+  },
+  {
+    question: "Which of the following is a consequence of an excessively large vertical dimension at occlusion?",
+    options: ["Thin-lipped appearance", "Increased rate of residual ridge resorption", "Prominence of the chin", "Excessive wrinkles at the corners of the mouth"],
+    answer: 1
+  },
+  {
+    question: "The 'facial expression' method for determining VDR relies on:",
+    options: ["The patient's ability to relax on command", "The dentist's experience in noticing relaxed facial features", "Measuring the distance between anatomic landmarks", "The pronunciation of specific words"],
+    answer: 1
+  },
+  {
+    question: "In the 'swallowing threshold' method, what is used to stimulate the flow of saliva to encourage repeated swallowing?",
+    options: ["A sip of water", "A piece of candy", "A sour-tasting liquid", "Repeated instructions from the dentist"],
+    answer: 1
+  },
+  {
+    question: "Why is it essential to record the vertical dimension at rest (VDR) quickly?",
+    options: ["Because the patient will become fatigued", "Because the patient cannot maintain the physiological rest position indefinitely", "Because the recording material has a short setting time", "Because the dentist must work efficiently"],
+    answer: 1
+  },
+  {
+    question: "What is the main disadvantage of using the 'parallelism of ridges' method for determining VDO?",
+    options: ["It requires a special device called a bimeter", "It is unreliable in patients with a history of periodontal disease or teeth lost at different times", "It is only applicable in dentulous patients", "It is a very time-consuming procedure"],
+    answer: 1
+  },
+  {
+    question: "Which phonetic sounds are used to determine the 'closest speaking space' as described by Silverman Meyer?",
+    options: ["m, b, p", "f, v", "ch, s, j", "t, d, n"],
+    answer: 2
+  },
+  {
+    question: "The 'power point' method for determining VDO utilizes a device called a:",
+    options: ["Boley's gauge", "Willis gauge", "Bimeter", "Facebook"],
+    answer: 2
+  },
+  {
+    question: "According to the lecture, what is the relationship between the mandible and maxilla at occlusion, in terms of ridge parallelism?",
+    options: ["The mandible is at a 5-degree angle to the maxilla", "The mandible is parallel to the maxilla", "The ridges are always non-parallel", "The mandible is slightly anterior to the maxilla"],
+    answer: 1
+  },
+  {
+    question: "Which of the following is a problem associated with an excessively large VDO?",
+    options: ["Prominence of the mandible and chin", "Neuralgia", "Loss of biting power", "Inability to open the mouth widely"],
+    answer: 3
+  },
+  {
+    question: "The method of using pre-extraction profile photographs as a record involves:",
+    options: ["Tracing the outline of the mandible", "Enlarging the photograph to life-size and comparing measurements on the photograph to those on the face", "Superimposing the photograph on a radiograph", "Using the photograph to select artificial teeth"],
+    answer: 1
+  },
+  {
+    question: "In patients with prolonged edentulousness, why is it important not to fabricate dentures using the habitual rest position?",
+    options: ["It causes severe pain", "It is the same as the physiological rest position", "The dentist needs to re-determine the physiological rest position", "It leads to increased salivary flow"],
+    answer: 2
+  },
+  {
+    question: "The interocclusal distance measured between the wax rims when the mandible is in its physiological rest position should be used to assess:",
+    options: ["The centric relation", "The freeway space", "The patient's esthetics", "The condylar inclination"],
+    answer: 1
+  },
+  {
+    question: "All of the following are physiological methods for measuring VDO EXCEPT:",
+    options: ["Swallowing threshold", "Phonetics", "Measurement from former dentures", "Physiological rest position"],
+    answer: 2
+  },
+  {
+    question: "The sensation of 'bulky dentures' is most likely to occur when the VDO is recorded:",
+    options: ["Too small", "Too large", "Correctly, but the teeth are set too far buccally", "Correctly, but the denture base is too thick"],
+    answer: 1
   }
 ];
