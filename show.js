a2a22fca2f907fc66ce9ddca8caa319079d1381c
mahmoud adil <mahmod.adil2001@gmail.com>
2026-02-25 19:41:57 +0300

    Toggle visibility for generalsurgery lecture 11 version 1

--- show.js
+++ show.js
@@ -248,7 +248,9 @@ export const visibleLectures = {
             3,
             4
         ],
-        "11": [],
+        "11": [
+            1
+        ],
         "twentynine": [
             4
         ]
