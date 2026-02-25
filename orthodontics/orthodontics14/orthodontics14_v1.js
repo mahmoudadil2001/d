732e1c4ed077e4ae060857013927eca8377587f3
mahmoud adil <mahmod.adil2001@gmail.com>
2026-02-25 19:51:33 +0300

    Update content for orthodontics14_v1.js

--- orthodontics/orthodontics14/orthodontics14_v1.js
+++ orthodontics/orthodontics14/orthodontics14_v1.js
@@ -1,7 +1,77 @@
 export const questions = [
   {
-    question: "Sample question for Orthodontics Lecture 14, Version 1?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
-    answer: 0
+    question: "What is anchorage in orthodontics?",
+    options: ["The force applied to move teeth", "The resistance offered by anchor units to unwanted tooth movement", "The movement of teeth", "The bracket system used"],
+    answer: 1
+  },
+  {
+    question: "According to Graber, anchorage is defined as:",
+    options: ["The site of delivery from which a force is exerted", "The nature and degree of resistance to displacement offered by an anatomic unit", "The movement of teeth", "The appliance used"],
+    answer: 1
+  },
+  {
+    question: "Which structures can be used as extra-oral anchorage sources?",
+    options: ["Teeth only", "Palate only", "Occiput and back of neck", "Tongue only"],
+    answer: 2
+  },
+  {
+    question: "In simple anchorage, the combined root surface area of anchor teeth should ideally be:",
+    options: ["Equal to teeth to be moved", "Two times that of teeth to be moved", "Three times that of teeth to be moved", "Half of teeth to be moved"],
+    answer: 1
+  },
+  {
+    question: "Which type of anchorage provides greater resistance than simple anchorage?",
+    options: ["Reciprocal anchorage", "Primary anchorage", "Stationary anchorage", "Compound anchorage"],
+    answer: 2
+  },
+  {
+    question: "Reciprocal anchorage involves:",
+    options: ["One tooth moving", "Two mal-posed units with equal and opposite forces", "Only posterior teeth", "Only anterior teeth"],
+    answer: 1
+  },
+  {
+    question: "Correction of midline diastema is an example of:",
+    options: ["Simple anchorage", "Stationary anchorage", "Reciprocal anchorage", "Absolute anchorage"],
+    answer: 2
+  },
+  {
+    question: "Intra-maxillary anchorage means resistance units are:",
+    options: ["In different jaws", "Outside the mouth", "Within the same jaw", "In the neck"],
+    answer: 2
+  },
+  {
+    question: "Class II elastics are an example of:",
+    options: ["Intra-maxillary anchorage", "Inter-maxillary anchorage", "Extra-oral anchorage", "Simple anchorage"],
+    answer: 1
+  },
+  {
+    question: "Which root form offers the least anchorage?",
+    options: ["Triangular", "Flat", "Round", "I-shaped"],
+    answer: 2
+  },
+  {
+    question: "Triangular root form is seen in:",
+    options: ["Mandibular incisors", "Bicuspids", "Maxillary central incisors and canines", "All molars"],
+    answer: 2
+  },
+  {
+    question: "Maxillary canines provide good anchorage because they have:",
+    options: ["Multiple roots", "Short roots", "Long roots", "Flat roots"],
+    answer: 2
+  },
+  {
+    question: "Multi-rooted teeth provide:",
+    options: ["Less anchorage than single-rooted teeth", "Equal anchorage to single-rooted teeth", "Greater anchorage than single-rooted teeth", "No anchorage"],
+    answer: 2
+  },
+  {
+    question: "The Nance palatal button uses anchorage from:",
+    options: ["Back of neck", "Hard palate", "Teeth only", "Chin"],
+    answer: 1
+  },
+  {
+    question: "Lip bumper appliance utilizes forces from:",
+    options: ["Tongue", "Hypertonic labial musculature", "Palate", "Neck muscles"],
+    answer: 1
   }
 ];
