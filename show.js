a630df06ad1a8760cb6a7e4851aafa4fa141e0fd
mahmoud adil <mahmod.adil2001@gmail.com>
2026-02-25 20:51:59 +0300

    Toggle visibility for pedodontics lecture 11 version 4

--- show.js
+++ show.js
@@ -685,7 +685,8 @@ export const visibleLectures = {
         "11": [
             1,
             2,
-            3
+            3,
+            4
         ],
         "twentysix": [
             1,
