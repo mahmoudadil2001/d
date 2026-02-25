f261a6d69bc72fb2e11f037fd0cd7c62365e0bce
mahmoud adil <mahmod.adil2001@gmail.com>
2026-02-25 20:12:14 +0300

    Update lecture name for prosthodontics lecture 16

--- lectureNames.js
+++ lectureNames.js
@@ -303,7 +303,7 @@ export const lectureNames = {
         "13": "diagnosis and treatment plan for complete denture🟦🟩",
         "14": "Impressions for Complete Dentures P.1",
         "15": "Impressions for Complete Dentures P.2",
-        "16": "Unknown",
+        "16": "Vertical jaw relation",
         "17": "Unknown",
         "18": "Unknown",
         "19": "Unknown",
