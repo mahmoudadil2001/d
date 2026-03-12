483822d5210db824d8b6c72a6cc23d9e6dae69a6
mahmoud adil <mahmod.adil2001@gmail.com>
2026-03-12 13:00:24 +0300

    Update lecture name for oralpathology lecture 13

--- lectureNames.js
+++ lectureNames.js
@@ -140,7 +140,7 @@ export const lectureNames = {
         "10": "malignant Bone tumors",
         "11": "odontogenic cysts",
         "12": "Odontogenic tumor",
-        "13": "Unknown",
+        "13": "developmental defects of oral region",
         "14": "Unknown",
         "15": "Unknown",
         "16": "Unknown",
