0e7209f93a608987f77cdd78a7b53e0ea05f29d9
mahmoud adil <mahmod.adil2001@gmail.com>
2026-03-12 13:33:12 +0300

    Update content for oralsurgery23_v2.js

--- oralsurgery/oralsurgery23/oralsurgery23_v2.js
+++ oralsurgery/oralsurgery23/oralsurgery23_v2.js
@@ -1,7 +1,152 @@
 export const questions = [
   {
-    question: "Sample question for Oralsurgery Lecture 23, Version 2?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
-    answer: 0
+    question: "Which of the following is listed as a cause of maxillary canine impaction?",
+    options: [
+      "Early eruption of permanent teeth",
+      "Missing lateral incisor which acts as a guide for the canine",
+      "Excessive space in the arch",
+      "Premature loss of first molars"
+    ],
+    answer: 1
+  },
+  {
+    question: "In Archer's classification, Class I impacted canines are located in the:",
+    options: [
+      "Labial surface",
+      "Palate",
+      "Alveolar process",
+      "Edentulous maxilla"
+    ],
+    answer: 1
+  },
+  {
+    question: "According to Archer's classification, Class IV impacted canines are located:",
+    options: [
+      "In the palate horizontally",
+      "On the labial surface vertically",
+      "Within the alveolar process, usually vertically between incisor and first premolar",
+      "In edentulous maxilla"
+    ],
+    answer: 2
+  },
+  {
+    question: "The lateral incisor may be retroclined if the impacted canine is located:",
+    options: [
+      "Labially",
+      "Palatally",
+      "Mesially",
+      "Distally"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which radiographic view provides a detailed view of the tooth, surrounding bone, root formation, and presence of root resorption of adjacent teeth?",
+    options: [
+      "OPG",
+      "Lateral skull view",
+      "Periapical radiograph",
+      "Vertex occlusal"
+    ],
+    answer: 2
+  },
+  {
+    question: "According to the SLOB rule, if the tooth moves in the opposite direction to the tube shift, it is localized:",
+    options: [
+      "Palatally",
+      "Buccally",
+      "Mesially",
+      "In the sinus"
+    ],
+    answer: 1
+  },
+  {
+    question: "The periapical-occlusal method for localization uses which two views?",
+    options: [
+      "OPG and lateral skull",
+      "Standard periapical and an occlusal view",
+      "Vertex and panoramic",
+      "CBCT and periapical"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which imaging modality is mentioned as being useful for localizing impacted canines, providing three-dimensional information?",
+    options: [
+      "OPG",
+      "Periapical",
+      "CBCT",
+      "Lateral skull view"
+    ],
+    answer: 2
+  },
+  {
+    question: "Retention or leaving an impacted canine in situ is indicated when:",
+    options: [
+      "The tooth is symptomatic",
+      "Extraction may lead to damage to adjacent teeth and it is aesthetically acceptable",
+      "The patient is young",
+      "There is associated infection"
+    ],
+    answer: 1
+  },
+  {
+    question: "For buccally impacted canines, surgical exposure is approached through which type of flap?",
+    options: [
+      "Palatal envelope flap",
+      "3-sided buccal flap",
+      "Envelope flap",
+      "Semilunar flap"
+    ],
+    answer: 1
+  },
+  {
+    question: "In palatally positioned canines where a bracket is not attached immediately, the window is packed until it epithelializes for:",
+    options: [
+      "1 week",
+      "2-3 weeks",
+      "1 month",
+      "6 weeks"
+    ],
+    answer: 1
+  },
+  {
+    question: "In buccal approaches for impacted canines, the flap is sutured above the crown to ensure eruption into an area of:",
+    options: [
+      "Non-keratinized mucosa",
+      "Keratinized mucosa",
+      "Alveolar mucosa",
+      "Buccal fat pad"
+    ],
+    answer: 1
+  },
+  {
+    question: "After autotransplantation, the transplanted tooth should be splinted for approximately:",
+    options: [
+      "One week",
+      "One month",
+      "Three months",
+      "Six months"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which of the following is an indication for extraction of an impacted canine?",
+    options: [
+      "Asymptomatic and aesthetically acceptable",
+      "When there is resorption of roots of adjacent teeth",
+      "When there is adequate space in the arch",
+      "When the patient is under 13 years"
+    ],
+    answer: 1
+  },
+  {
+    question: "Impacted mandibular canines are mostly located:",
+    options: [
+      "Lingually",
+      "Buccally",
+      "Palatally",
+      "Within the mandibular canal"
+    ],
+    answer: 1
   }
 ];
