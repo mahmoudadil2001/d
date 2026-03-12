9c8ed64252aba946fe7a9b2d9086ada41fb26f94
mahmoud adil <mahmod.adil2001@gmail.com>
2026-03-12 13:33:05 +0300

    Update content for oralsurgery23_v1.js

--- oralsurgery/oralsurgery23/oralsurgery23_v1.js
+++ oralsurgery/oralsurgery23/oralsurgery23_v1.js
@@ -1,7 +1,152 @@
 export const questions = [
   {
-    question: "Sample question for Oralsurgery Lecture 23, Version 1?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
-    answer: 0
+    question: "impacted maxillary canines are often associated with other dental abnormalities, supporting which theory of impaction?",
+    options: [
+      "Failure of root resorption theory",
+      "Genetic theory",
+      "Scar tissue theory",
+      "Missing lateral incisor theory"
+    ],
+    answer: 1
+  },
+  {
+    question: "If the maxillary canine fails to erupt by what age should its position be investigated?",
+    options: [
+      "10 years",
+      "13 years",
+      "16 years",
+      "18 years"
+    ],
+    answer: 1
+  },
+  {
+    question: "According to Archer's (1975) classification, which class describes impacted canines located on the labial surface?",
+    options: [
+      "Class I",
+      "Class II",
+      "Class III",
+      "Class IV"
+    ],
+    answer: 1
+  },
+  {
+    question: "Clinical examination reveals that palatal impaction of the maxillary canine is:",
+    options: [
+      "Less common than buccal impaction",
+      "More common than buccal impaction",
+      "Equal in frequency to buccal impaction",
+      "Never occurs"
+    ],
+    answer: 1
+  },
+  {
+    question: "The buccal object rule (parallax method) for localizing impacted canines was described by:",
+    options: [
+      "Archer (1975)",
+      "Clark (1909)",
+      "Ward (1956)",
+      "Pell and Gregory"
+    ],
+    answer: 1
+  },
+  {
+    question: "In the SLOB rule for localization, if the tooth moves in the same direction as the tube shift, it is localized:",
+    options: [
+      "Buccally",
+      "Palatally",
+      "Mesially",
+      "Distally"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which radiographic view produces an axial view of the incisors to demonstrate buccopalatal localization of the canine?",
+    options: [
+      "Periapical view",
+      "Vertex occlusal projection",
+      "OPG",
+      "Lateral skull view"
+    ],
+    answer: 1
+  },
+  {
+    question: "On OPG, a palatally impacted canine appears:",
+    options: [
+      "Smaller than normal",
+      "Magnified",
+      "Distorted",
+      "Invisible"
+    ],
+    answer: 1
+  },
+  {
+    question: "Which of the following is NOT a criterion that must be fulfilled for surgical exposure and orthodontic traction of an impacted canine?",
+    options: [
+      "Adequate space in the arch",
+      "Unobstructed path of eruption",
+      "The tooth should have a closed apex",
+      "Timing close to normal eruption time"
+    ],
+    answer: 2
+  },
+  {
+    question: "For palatally impacted canines, surgical exposure is approached through which type of flap?",
+    options: [
+      "Buccal 3-sided flap",
+      "Palatal envelope flap",
+      "Semilunar flap",
+      "Trapezoidal flap"
+    ],
+    answer: 1
+  },
+  {
+    question: "During surgical exposure of an impacted canine, care should be taken not to expose which structure to prevent increased incidence of external root resorption?",
+    options: [
+      "Root apex",
+      "Cementoenamel junction (CEJ)",
+      "Pulp chamber",
+      "Crown tip"
+    ],
+    answer: 1
+  },
+  {
+    question: "In autotransplantation of canines, success rate is increased when the unerupted tooth has:",
+    options: [
+      "A closed apex",
+      "An open apex",
+      "Fully formed root",
+      "No root development"
+    ],
+    answer: 1
+  },
+  {
+    question: "After autotransplantation, endodontic treatment should be performed as soon as possible after surgery, approximately:",
+    options: [
+      "1-2 weeks",
+      "6-8 weeks",
+      "3-4 months",
+      "6 months"
+    ],
+    answer: 1
+  },
+  {
+    question: "A possible complication of upper canine removal is palatal hematoma formation, which can be prevented by:",
+    options: [
+      "Pressure pack",
+      "An acrylic splint to support soft tissue",
+      "Antibiotics",
+      "Cold compress"
+    ],
+    answer: 1
+  },
+  {
+    question: "Impacted mandibular canines occur in approximately what percentage of the population?",
+    options: [
+      "2%",
+      "0.3%",
+      "5%",
+      "10%"
+    ],
+    answer: 1
   }
 ];
