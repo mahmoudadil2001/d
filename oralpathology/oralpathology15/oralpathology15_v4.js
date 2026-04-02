72d524044cdbe3596de6bd75e5f3c7eae26c696a
mahmoud adil <mahmod.adil2001@gmail.com>
2026-04-02 13:45:14 +0300

    Update content for oralpathology15_v4.js

--- oralpathology/oralpathology15/oralpathology15_v4.js
+++ oralpathology/oralpathology15/oralpathology15_v4.js
@@ -1,7 +1,77 @@
 export const questions = [
   {
-    question: "Sample question for Oralpathology Lecture 15, Version 4?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    question: "A vascular lesion caused by dilatation of a small superficial blood vessel is called:",
+    options: ["Petechia", "Ecchymosis", "Telangiectasia", "Hemangioma"],
+    answer: 2
+  },
+  {
+    question: "When nuclei are lost from the surface in hyperkeratosis, the condition is called:",
+    options: ["Hyperparakeratosis", "Hyperorthokeratosis", "Dyskeratosis", "Acanthosis"],
+    answer: 1
+  },
+  {
+    question: "An increase in the prickle cell layer is termed:",
+    options: ["Hyperkeratosis", "Acanthosis", "Dysplasia", "Acantholysis"],
+    answer: 1
+  },
+  {
+    question: "Which of the following is a predisposing factor for necrotizing ulcerative gingivitis?",
+    options: ["High carbohydrate diet", "Emotional stress", "Vitamin C excess", "Low protein intake"],
+    answer: 1
+  },
+  {
+    question: "Noma is also known as:",
+    options: ["Cancrum oris", "Herpetic stomatitis", "Actinomycosis", "Tuberculous ulcer"],
+    answer: 0
+  },
+  {
+    question: "Noma typically affects which population?",
+    options: ["Elderly adults", "Malnourished children", "Pregnant women", "Immunocompetent adults"],
+    answer: 1
+  },
+  {
+    question: "The characteristic lesion in primary syphilis is the:",
+    options: ["Gumma", "Mucous patch", "Chancre", "Condylomata lata"],
+    answer: 2
+  },
+  {
+    question: "Oral chancre appears in approximately what percentage of primary syphilis cases?",
+    options: ["1-2%", "5-10%", "15-20%", "25-30%"],
+    answer: 1
+  },
+  {
+    question: "The secondary stage of acquired syphilis begins how long after the appearance of the chancre?",
+    options: ["1-2 weeks", "3-5 weeks", "6-8 weeks", "10-12 weeks"],
+    answer: 2
+  },
+  {
+    question: "Hutchinson's teeth are a characteristic finding in which form of syphilis?",
+    options: ["Primary syphilis", "Secondary syphilis", "Tertiary syphilis", "Congenital syphilis"],
+    answer: 3
+  },
+  {
+    question: "The most common oral manifestation of tuberculosis is:",
+    options: ["Osteomyelitis", "Periapical granuloma", "Tuberculous ulcer", "Scrofula"],
+    answer: 2
+  },
+  {
+    question: "In tuberculosis, the most common site for oral lesions is the:",
+    options: ["Lip", "Buccal mucosa", "Palate", "Dorsum of the tongue"],
+    answer: 3
+  },
+  {
+    question: "The basic microscopic lesion of tuberculosis is:",
+    options: ["Acute inflammation", "Granulomatous inflammation with caseous necrosis", "Suppurative inflammation", "Fibrous encapsulation"],
+    answer: 1
+  },
+  {
+    question: "In tuberculosis, fusion of macrophages results in the appearance of:",
+    options: ["Epithelioid cells", "Langerhans giant cells", "Plasma cells", "Neutrophils"],
+    answer: 1
+  },
+  {
+    question: "Actinomycosis is caused by which bacterium?",
+    options: ["Actinomyces israelii", "Mycobacterium tuberculosis", "Treponema pallidum", "Candida albicans"],
     answer: 0
   }
 ];
