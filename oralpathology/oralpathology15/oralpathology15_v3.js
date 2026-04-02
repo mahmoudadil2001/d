5874aa7cab2959aedda260f67275b875c4f7177f
mahmoud adil <mahmod.adil2001@gmail.com>
2026-04-02 13:45:09 +0300

    Update content for oralpathology15_v3.js

--- oralpathology/oralpathology15/oralpathology15_v3.js
+++ oralpathology/oralpathology15/oralpathology15_v3.js
@@ -1,7 +1,77 @@
 export const questions = [
   {
-    question: "Sample question for Oralpathology Lecture 15, Version 3?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    question: "A lesion characterized by loss of surface epithelium appearing depressed or excavated is called an:",
+    options: ["Erosion", "Ulcer", "Fissure", "Plaque"],
+    answer: 1
+  },
+  {
+    question: "A nonelevated area of hemorrhage larger than a petechia is called an:",
+    options: ["Ecchymosis", "Telangiectasia", "Petechia", "Purpura"],
     answer: 0
+  },
+  {
+    question: "An increase in the thickness of the epithelium from surface to basal cell layer is called:",
+    options: ["Hyperkeratosis", "Hyperplasia", "Dysplasia", "Acantholysis"],
+    answer: 1
+  },
+  {
+    question: "Epithelial dysplasia generally indicates:",
+    options: ["Normal variation", "Inflammatory change", "Premalignant change", "Benign hyperplasia"],
+    answer: 2
+  },
+  {
+    question: "Which condition shows acantholysis as a key histopathologic feature?",
+    options: ["Lichen planus", "Pemphigus vulgaris", "Erythema multiforme", "Candidiasis"],
+    answer: 1
+  },
+  {
+    question: "In primary herpetic infection, lesions heal:",
+    options: ["With scarring", "Without scar formation", "With hyperpigmentation", "With atrophy"],
+    answer: 1
+  },
+  {
+    question: "What percentage of the population has antibodies to HSV?",
+    options: ["Up to 50%", "Up to 70%", "Up to 90%", "Up to 99%"],
+    answer: 2
+  },
+  {
+    question: "What percentage of HSV-antibody-positive individuals may develop secondary herpes?",
+    options: ["Up to 20%", "Up to 40%", "Up to 60%", "Up to 80%"],
+    answer: 1
+  },
+  {
+    question: "Secondary herpes on the vermilion and surrounding skin is usually referred to as:",
+    options: ["Herpetic stomatitis", "Herpes labialis", "Herpetic gingivitis", "Herpetic pharyngitis"],
+    answer: 1
+  },
+  {
+    question: "Measles is caused by a member of which virus family?",
+    options: ["Togavirus", "Paramyxovirus", "Herpesvirus", "Picornavirus"],
+    answer: 1
+  },
+  {
+    question: "Koplik's spots are pathognomonic for which disease?",
+    options: ["Rubella", "Measles", "Chickenpox", "Scarlet fever"],
+    answer: 1
+  },
+  {
+    question: "In measles, Koplik's spots generally precede the skin rash by:",
+    options: ["1-2 hours", "1-2 days", "3-5 days", "1 week"],
+    answer: 1
+  },
+  {
+    question: "Necrotizing ulcerative gingivitis is characterized by painful necrosis of the:",
+    options: ["Attached gingiva", "Interdental papillae and gingival margins", "Buccal mucosa", "Palatal mucosa"],
+    answer: 1
+  },
+  {
+    question: "The pseudomembrane in necrotizing ulcerative gingivitis is typically:",
+    options: ["White and firmly attached", "Yellow and easily removed", "Gray in color", "Red and hemorrhagic"],
+    answer: 2
+  },
+  {
+    question: "The best therapy for necrotizing ulcerative gingivitis in the acute phase includes systemic metronidazole and topical:",
+    options: ["Antibiotics", "Corticosteroids", "Oxygen-releasing agents", "Antifungals"],
+    answer: 2
   }
 ];
