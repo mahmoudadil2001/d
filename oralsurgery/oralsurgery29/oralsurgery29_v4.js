30fb7e00be7eef12a205d87407061e6ae5201016
mahmoud adil <mahmod.adil2001@gmail.com>
2026-04-02 14:22:41 +0300

    Update content for oralsurgery29_v4.js

--- oralsurgery/oralsurgery29/oralsurgery29_v4.js
+++ oralsurgery/oralsurgery29/oralsurgery29_v4.js
@@ -1,7 +1,77 @@
 export const questions = [
   {
-    question: "Sample question for Oralsurgery Lecture 29, Version 4?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    question: "Which of the following is an advantage of CT scanning?",
+    options: ["No ionizing radiation", "Quick imaging, scan time less than 1 minute", "Cannot evaluate soft tissue", "High superimposition"],
+    answer: 1
+  },
+  {
+    question: "CT scanning gives a 3D image, allowing determination of:",
+    options: ["Tooth color only", "Exact site and size of lesion and relation to adjacent structures", "Only bone density", "Only soft tissue texture"],
+    answer: 1
+  },
+  {
+    question: "Which of the following is NOT an application of CT scanning?",
+    options: ["Head and facial skeleton fractures", "Osteolytic lesions", "Measuring blood pressure", "Cervical lymphadenopathy"],
+    answer: 2
+  },
+  {
+    question: "CT angiography uses which agent to visualize blood vessels?",
+    options: ["Saline", "Contrast agent", "Air", "Barium"],
+    answer: 1
+  },
+  {
+    question: "The effective dose of a panoramic radiograph ranges from:",
+    options: ["<1 μSv", "1-10 μSv", "2.7-24 μSv", "30-50 μSv"],
+    answer: 2
+  },
+  {
+    question: "The effective dose of CBCT (craniofacial) median is:",
+    options: ["61 μSv", "87 μSv", "280 μSv", "1410 μSv"],
+    answer: 1
+  },
+  {
+    question: "In ultrasonography, the term 'hyperechoic' refers to tissues that:",
+    options: ["Allow all sound waves to pass", "Reflect sound waves completely", "Partially allow sound waves", "Absorb all sound waves"],
+    answer: 1
+  },
+  {
+    question: "Ultrasonography is useful for examination of all of the following EXCEPT:",
+    options: ["Thyroid gland", "Salivary glands", "Lungs (due to air)", "Cervical lymphadenopathy"],
+    answer: 2
+  },
+  {
+    question: "Ultrasonography aids in differentiation of:",
+    options: ["Solid vs. cystic masses", "Bone vs. cartilage", "Arteries vs. veins only", "Nerves vs. muscles"],
     answer: 0
+  },
+  {
+    question: "In digital radiography, images can be stored for:",
+    options: ["Only 1 year", "Only 5 years", "Long period of time", "30 days only"],
+    answer: 2
+  },
+  {
+    question: "A disadvantage of digital radiography is that digital images are prone to:",
+    options: ["Chemical hazards", "Film processing errors", "Intentional manipulation for misuse", "High radiation dose"],
+    answer: 2
+  },
+  {
+    question: "Which of the following is an advantage of digital radiography?",
+    options: ["Cannot adjust contrast", "Requires chemical processing", "Easily transferred between offices", "Expensive but worth it"],
+    answer: 2
+  },
+  {
+    question: "Conventional radiography gives a:",
+    options: ["Three-dimensional record", "Two-dimensional record", "Color image", "Real-time video"],
+    answer: 1
+  },
+  {
+    question: "Which of the following is an intraoral radiographical view?",
+    options: ["OPG", "Cephalometric", "Occlusal", "Occipitomental"],
+    answer: 2
+  },
+  {
+    question: "The term 'effective dose' is used when referring to radiation risk:",
+    options: ["To a specific organ", "Only to bone marrow", "Averaged over the entire body", "To the skin only"],
+    answer: 2
   }
 ];
