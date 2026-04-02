1edf15cf6ff56af5c7f79f4224b0692fee550599
mahmoud adil <mahmod.adil2001@gmail.com>
2026-04-02 13:53:45 +0300

    Update content for periodontology17_v4.js

--- periodontology/periodontology17/periodontology17_v4.js
+++ periodontology/periodontology17/periodontology17_v4.js
@@ -1,7 +1,77 @@
 export const questions = [
   {
-    question: "Sample question for Periodontology Lecture 17, Version 4?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    question: "Which index is used to measure oral cleanliness?",
+    options: ["OHI-S", "DMFT", "GI", "CPITN"],
+    answer: 0
+  },
+  {
+    question: "Which index measures plaque at gingival margin?",
+    options: ["Plaque Index", "DMFT", "GI", "CPI"],
+    answer: 0
+  },
+  {
+    question: "Which index is used for caries epidemiology?",
+    options: ["DMFT", "GI", "PI", "CI"],
+    answer: 0
+  },
+  {
+    question: "Which index includes bleeding on probing?",
+    options: ["GI", "DMFT", "PI", "CI"],
+    answer: 0
+  },
+  {
+    question: "Which index includes calculus and debris?",
+    options: ["OHI-S", "GI", "PI", "DMFT"],
+    answer: 0
+  },
+  {
+    question: "Which index uses codes 0–4?",
+    options: ["CPITN", "DMFT", "PI", "GI"],
+    answer: 0
+  },
+  {
+    question: "Which index is irreversible?",
+    options: ["DMFT", "GI", "PI", "OHI-S"],
+    answer: 0
+  },
+  {
+    question: "Which index assesses gingival bleeding severity?",
+    options: ["GI", "DMFT", "PI", "CI"],
+    answer: 0
+  },
+  {
+    question: "Which index uses sextants?",
+    options: ["CPITN", "GI", "PI", "DMFT"],
+    answer: 0
+  },
+  {
+    question: "Which index measures plaque only?",
+    options: ["PI", "GI", "DMFT", "CI"],
+    answer: 0
+  },
+  {
+    question: "Which index is best for hygiene instruction?",
+    options: ["OHI-S", "DMFT", "CPI", "Russell Index"],
+    answer: 0
+  },
+  {
+    question: "Which index records decayed teeth?",
+    options: ["DMFT", "GI", "PI", "CI"],
+    answer: 0
+  },
+  {
+    question: "Which index evaluates periodontal pockets?",
+    options: ["CPITN", "PI", "GI", "DMFT"],
+    answer: 0
+  },
+  {
+    question: "Which index measures gingival inflammation?",
+    options: ["GI", "DMFT", "PI", "CI"],
+    answer: 0
+  },
+  {
+    question: "Which index is simplest to use?",
+    options: ["OHI-S", "GI", "PI", "CPI"],
     answer: 0
   }
 ];
