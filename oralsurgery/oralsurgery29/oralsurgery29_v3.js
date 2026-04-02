7d2ff84a7249312e36254d7cf293b7c638ff0d26
mahmoud adil <mahmod.adil2001@gmail.com>
2026-04-02 14:22:35 +0300

    Update content for oralsurgery29_v3.js

--- oralsurgery/oralsurgery29/oralsurgery29_v3.js
+++ oralsurgery/oralsurgery29/oralsurgery29_v3.js
@@ -1,7 +1,77 @@
 export const questions = [
   {
-    question: "Sample question for Oralsurgery Lecture 29, Version 3?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
-    answer: 0
+    question: "Which of the following is an advantage of CT scanning?",
+    options: ["Ionizing radiation", "Expensive", "Non-invasive and gives 3D image", "Requires sedation for children"],
+    answer: 2
+  },
+  {
+    question: "CT can differentiate various body structures and demonstrates their relationship without:",
+    options: ["Radiation", "Contrast", "Superimposition", "Software"],
+    answer: 2
+  },
+  {
+    question: "Which of the following is a disadvantage of CT scanning?",
+    options: ["No ionizing radiation", "Low cost", "Artifact scatter due to dental amalgam", "No contrast reactions"],
+    answer: 2
+  },
+  {
+    question: "Artifact scatter on CT can be caused by:",
+    options: ["Patient movement", "Dental amalgam fillings and metallic crowns", "Low radiation dose", "Soft tissue edema"],
+    answer: 1
+  },
+  {
+    question: "CT is an effective means of guiding the placement of:",
+    options: ["Surgical drills", "Fine needle aspiration (FNA) for biopsy", "Dental implants", "Orthodontic brackets"],
+    answer: 1
+  },
+  {
+    question: "CBCT has an advantage over conventional CT by giving a 3D image and exposing tissues to:",
+    options: ["Higher radiation dose", "Relatively small doses of radiation", "No radiation", "Magnetic fields"],
+    answer: 1
+  },
+  {
+    question: "The radiation dose of medical CT exceeds that of:",
+    options: ["CBCT only", "Conventional panoramic radiography only", "Both CBCT and conventional panoramic radiography", "Ultrasound"],
+    answer: 2
+  },
+  {
+    question: "CBCT dento-alveolar effective dose median is equivalent to how many days of natural background radiation?",
+    options: ["5 days", "10 days", "14 days", "4 months"],
+    answer: 1
+  },
+  {
+    question: "Multislice CT effective dose range is equivalent to how many months of natural background radiation?",
+    options: ["2-8 days", "2-8 weeks", "2-8 months", "2-8 years"],
+    answer: 2
+  },
+  {
+    question: "Which of the following is an advantage of CT scanning over conventional x-ray?",
+    options: ["Lower cost", "No ionizing radiation", "Contrast resolution is much better", "No artifacts"],
+    answer: 2
+  },
+  {
+    question: "CT scanning is used for all of the following EXCEPT:",
+    options: ["Evaluation of bony structure", "Evaluation of soft tissue", "Detection of dental caries only", "Postoperative evaluation"],
+    answer: 2
+  },
+  {
+    question: "CT scanning can be used to evaluate:",
+    options: ["Only bone", "Only soft tissue", "Intracranial lesions such as hematoma and tumors", "Only teeth"],
+    answer: 2
+  },
+  {
+    question: "The scan time for modern CT scanners is:",
+    options: ["Several minutes", "10-50 seconds", "1-2 hours", "30-60 minutes"],
+    answer: 1
+  },
+  {
+    question: "One advantage of fast CT scan time is that it:",
+    options: ["Increases radiation dose", "Minimizes motion artifacts", "Reduces image quality", "Requires more contrast"],
+    answer: 1
+  },
+  {
+    question: "3D reconstruction from CT scan data takes approximately:",
+    options: ["5-10 minutes", "20-30 minutes", "1 hour", "2 hours"],
+    answer: 1
   }
 ];
