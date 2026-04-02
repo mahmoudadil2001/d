b7276d05c14d5190170ceda5de363d1d3ac3216a
mahmoud adil <mahmod.adil2001@gmail.com>
2026-04-02 14:21:18 +0300

    Update content for oralsurgery28_v4.js

--- oralsurgery/oralsurgery28/oralsurgery28_v4.js
+++ oralsurgery/oralsurgery28/oralsurgery28_v4.js
@@ -1,7 +1,77 @@
 export const questions = [
   {
-    question: "Sample question for Oralsurgery Lecture 28, Version 4?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    question: "CT is less expensive than MRI; approximately how much does CT cost compared to MRI?",
+    options: ["One quarter the price", "Half the price", "Equal price", "Double the price"],
+    answer: 1
+  },
+  {
+    question: "CT scan time for a full scan is approximately:",
+    options: ["30-60 minutes", "5-10 minutes", "1-2 hours", "10-15 seconds"],
+    answer: 1
+  },
+  {
+    question: "Which modality seldom creates claustrophobia?",
+    options: ["MRI", "CT", "PET", "SPECT"],
+    answer: 1
+  },
+  {
+    question: "Radionuclide imaging emits which type of radioactivity?",
+    options: ["Alpha rays", "Beta rays", "Gamma rays", "X-rays"],
+    answer: 2
+  },
+  {
+    question: "Uptake of radionuclide is monitored with a:",
+    options: ["X-ray tube", "Gamma camera", "MRI coil", "Ultrasound probe"],
+    answer: 1
+  },
+  {
+    question: "Cold spots on scintigraphy suggest all of the following EXCEPT:",
+    options: ["Decreased metabolic activity", "Tumor metastases", "Increased vascularity", "Infection"],
+    answer: 2
+  },
+  {
+    question: "PET can detect positron-emitting radionuclides to study a variety of:",
+    options: ["Anatomic structures", "Metabolic processes", "Bone density", "Blood pressure"],
+    answer: 1
+  },
+  {
+    question: "The disadvantage of PET regarding exact localization is overcome by:",
+    options: ["Higher dose", "Hybrid systems like PET-CT", "Longer scan time", "Different radiopharmaceuticals"],
+    answer: 1
+  },
+  {
+    question: "Radionuclide imaging is NOT preferred for women who are:",
+    options: ["Over 50 years old", "Pregnant or breastfeeding", "Diabetic", "Hypertensive"],
+    answer: 1
+  },
+  {
+    question: "Which MRI application is used to evaluate the TMJ disc?",
+    options: ["Tumor detection", "Angiography", "Evaluation of TMJ disc deformities", "Cranial neuropathy"],
+    answer: 2
+  },
+  {
+    question: "MRI can be used to guide the placement of FNA for biopsy using:",
+    options: ["Ferromagnetic needles", "Non-ferromagnetic needles", "Standard surgical needles", "No needles"],
+    answer: 1
+  },
+  {
+    question: "The ability of MRI to differentiate a nerve from fat and CSF makes it the procedure of choice to visualize:",
+    options: ["Cranial nerves", "Peripheral nerves", "Autonomic nerves", "Spinal nerves only"],
     answer: 0
+  },
+  {
+    question: "CT can be used in patients with metal in their body, while MRI:",
+    options: ["Can also be used", "Cannot be used due to dislodgement risk", "Is preferred", "Requires lower field strength"],
+    answer: 1
+  },
+  {
+    question: "MRI can be used to evaluate the larynx, nasopharynx, and:",
+    options: ["Oral cavity", "Paranasal sinuses", "Trachea", "Esophagus"],
+    answer: 1
+  },
+  {
+    question: "MRI angiography is used to evaluate:",
+    options: ["Bone structure", "Blood vessels", "Nerve pathways", "Muscle fibers"],
+    answer: 1
   }
 ];
