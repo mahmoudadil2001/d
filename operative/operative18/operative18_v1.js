76071081ea11d3b1f35f38d46e3de0049383ac16
mahmoud adil <mahmod.adil2001@gmail.com>
2026-04-09 15:30:49 +0300

    Update content for operative18_v1.js

--- operative/operative18/operative18_v1.js
+++ operative/operative18/operative18_v1.js
@@ -1,7 +1,78 @@
+// Version 1
 export const questions = [
   {
-    question: "Sample question for Operative Lecture 18, Version 1?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    question: "What is an advantage of using Er,Cr:YSGG laser for cavity preparation?",
+    options: ["Requires local anesthesia always", "Can remove caries without removing sound enamel below the lesion", "Produces more microleakage", "Cannot prepare Class I cavities"],
+    answer: 1
+  },
+  {
+    question: "When illuminated with argon laser light, how does carious tissue appear?",
+    options: ["Bright green color", "Dark, fiery, orange-red color", "Blue-white color", "Yellow color"],
+    answer: 1
+  },
+  {
+    question: "What is the wavelength and power of DIAGNOdent diode laser used for caries detection?",
+    options: ["488 nm, 5 mW", "514 nm, 2 mW", "655 nm, modulated, 1 mW peak power", "790 nm, 10 mW"],
+    answer: 2
+  },
+  {
+    question: "What is the optimal curing time for argon laser compared to conventional visible light?",
+    options: ["44 seconds for argon, 8 seconds for conventional", "8 seconds for argon, 44 seconds for conventional", "Both take 30 seconds", "Argon takes longer"],
+    answer: 1
+  },
+  {
+    question: "Which combination gives the best results in dental bleaching?",
+    options: ["Diode laser alone", "CO2 laser alone", "Argon laser irradiation combined with 35% hydrogen peroxide", "Bleaching agent alone"],
+    answer: 2
+  },
+  {
+    question: "What effect does laser treatment have on enamel surface?",
+    options: ["Smoothing of enamel", "Roughening through creation of bubble-like inclusions", "No change to enamel", "Complete removal of enamel"],
+    answer: 1
+  },
+  {
+    question: "How does laser treatment desensitize hypersensitive exposed dentine?",
+    options: ["By removing dentine", "By melting and recrystallization of dentine causing closure of open dental tubules", "By covering with composite", "By numbing the nerve"],
+    answer: 1
+  },
+  {
+    question: "What is the diameter of optic fiber used for laser root canal preparation?",
+    options: ["100 μm (equal to size 10 K-file)", "200 μm (equal to size 20 K-file)", "300 μm (equal to size 30 K-file)", "400 μm (equal to size 40 K-file)"],
+    answer: 1
+  },
+  {
+    question: "What is PIPS an acronym for?",
+    options: ["Photon Induced Photoacoustic Streaming", "Plasma Induced Photoacoustic System", "Photon Infrared Photoacoustic Streaming", "Pulsed Induced Photoacoustic Streaming"],
     answer: 0
+  },
+  {
+    question: "What energy and pulse duration settings are used for PIPS with Er:YAG laser?",
+    options: ["50 mJ, 100 microseconds", "20 mJ, 50 microseconds", "100 mJ, 20 microseconds", "10 mJ, 200 microseconds"],
+    answer: 1
+  },
+  {
+    question: "How is power density calculated?",
+    options: ["Power (watt) × area (cm²)", "Power (watt) / area (cm²)", "Area (cm²) / power (watt)", "Power (watt) + area (cm²)"],
+    answer: 1
+  },
+  {
+    question: "What determines the ability of a laser to vaporize, excise and coagulate biologic tissue?",
+    options: ["Energy density", "Wavelength", "Power density", "Exposure time"],
+    answer: 2
+  },
+  {
+    question: "Which type of radiation is absorbed at the cornea and can cause painful damage?",
+    options: ["Infrared radiation", "Visible light", "Ultraviolet radiation (200-400 nm)", "Radio waves"],
+    answer: 2
+  },
+  {
+    question: "What color safety glasses are required for use with Argon laser?",
+    options: ["Green glasses", "Amber colored glasses", "Clear glasses", "Red glasses"],
+    answer: 1
+  },
+  {
+    question: "What should be used to shield non-target oral tissues during laser procedures?",
+    options: ["Aluminum foil", "Dry gauze packs", "Wet gauze packs", "Plastic barriers"],
+    answer: 2
   }
 ];
