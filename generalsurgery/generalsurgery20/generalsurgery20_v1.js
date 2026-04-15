34f482e8d8a9f98e25930f35d10fc64366f491b5
mahmoud adil <mahmod.adil2001@gmail.com>
2026-04-15 20:33:06 +0300

    Update content for generalsurgery20_v1.js

--- generalsurgery/generalsurgery20/generalsurgery20_v1.js
+++ generalsurgery/generalsurgery20/generalsurgery20_v1.js
@@ -1,7 +1,77 @@
 export const questions = [
   {
-    question: "Sample question for Generalsurgery Lecture 20, Version 1?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    question: "Which test differentiates Gram-positive from Gram-negative bacteria?",
+    options: ["Ziehl-Neelsen stain", "Gram stain", "Albert stain", "Simple stain"],
+    answer: 1
+  },
+  {
+    question: "What color do Gram-negative bacteria appear?",
+    options: ["Purple", "Blue", "Pink", "Green"],
+    answer: 2
+  },
+  {
+    question: "Which structure increases bacterial virulence by preventing phagocytosis?",
+    options: ["Flagella", "Capsule", "Spore", "Cell wall"],
+    answer: 1
+  },
+  {
+    question: "Catalase enzyme breaks hydrogen peroxide into:",
+    options: ["Water and oxygen", "Water and CO2", "Oxygen and nitrogen", "Hydrogen and oxygen"],
+    answer: 0
+  },
+  {
+    question: "Which bacteria is catalase positive?",
+    options: ["Streptococcus", "Staphylococcus", "Enterococcus", "Lactobacillus"],
+    answer: 1
+  },
+  {
+    question: "Coagulase test is used to identify:",
+    options: ["E. coli", "Pseudomonas", "Staphylococcus aureus", "Klebsiella"],
+    answer: 2
+  },
+  {
+    question: "Oxidase positive bacteria produce which color?",
+    options: ["Yellow", "Red", "Purple", "Blue"],
+    answer: 2
+  },
+  {
+    question: "Which organism is oxidase positive?",
+    options: ["E. coli", "Pseudomonas", "Staphylococcus", "Streptococcus"],
+    answer: 1
+  },
+  {
+    question: "Urease test detects the breakdown of:",
+    options: ["Glucose", "Urea", "Protein", "Lipids"],
+    answer: 1
+  },
+  {
+    question: "A positive urease test turns medium into:",
+    options: ["Blue", "Pink", "Yellow", "Green"],
+    answer: 1
+  },
+  {
+    question: "Indole test detects breakdown of:",
+    options: ["Glucose", "Tryptophan", "Urea", "Lactose"],
+    answer: 1
+  },
+  {
+    question: "Which test uses Kovac’s reagent?",
+    options: ["Catalase", "Indole", "Oxidase", "Coagulase"],
+    answer: 1
+  },
+  {
+    question: "Ziehl-Neelsen stain is used for:",
+    options: ["Staphylococcus", "Mycobacterium", "E. coli", "Klebsiella"],
+    answer: 1
+  },
+  {
+    question: "Acid-fast bacteria appear:",
+    options: ["Blue", "Red", "Green", "Purple"],
+    answer: 1
+  },
+  {
+    question: "Which test detects citrate utilization?",
+    options: ["IMViC", "Oxidase", "Catalase", "Coagulase"],
     answer: 0
   }
 ];
