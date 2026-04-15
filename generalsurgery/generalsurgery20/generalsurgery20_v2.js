95705ef59ca9a56cb3a9805941c7648fc92147b7
mahmoud adil <mahmod.adil2001@gmail.com>
2026-04-15 20:33:12 +0300

    Update content for generalsurgery20_v2.js

--- generalsurgery/generalsurgery20/generalsurgery20_v2.js
+++ generalsurgery/generalsurgery20/generalsurgery20_v2.js
@@ -1,7 +1,77 @@
 export const questions = [
   {
-    question: "Sample question for Generalsurgery Lecture 20, Version 2?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    question: "Which staining method uses methylene blue?",
+    options: ["Simple stain", "Gram stain", "Ziehl stain", "Albert stain"],
+    answer: 0
+  },
+  {
+    question: "Gram-positive bacteria retain:",
+    options: ["Safranin", "Crystal violet", "Methylene blue", "Phenol red"],
+    answer: 1
+  },
+  {
+    question: "Which bacteria is Gram-negative?",
+    options: ["Staphylococcus", "Streptococcus", "E. coli", "Bacillus"],
+    answer: 2
+  },
+  {
+    question: "Which structure helps bacteria move?",
+    options: ["Capsule", "Flagella", "Spore", "Cell wall"],
+    answer: 1
+  },
+  {
+    question: "Catalase negative bacteria include:",
+    options: ["Staphylococcus", "Streptococcus", "Pseudomonas", "Proteus"],
+    answer: 1
+  },
+  {
+    question: "Coagulase positive result shows:",
+    options: ["Color change", "Clot formation", "Gas production", "No change"],
+    answer: 1
+  },
+  {
+    question: "Which test uses tetramethyl phenyl diamine?",
+    options: ["Catalase", "Oxidase", "Urease", "Indole"],
+    answer: 1
+  },
+  {
+    question: "Urease produces:",
+    options: ["Ammonia", "Oxygen", "CO2 only", "Hydrogen"],
+    answer: 0
+  },
+  {
+    question: "Which bacteria is urease positive?",
+    options: ["E. coli", "Proteus", "Streptococcus", "Enterococcus"],
+    answer: 1
+  },
+  {
+    question: "IMViC includes how many tests?",
+    options: ["2", "3", "4", "5"],
+    answer: 2
+  },
+  {
+    question: "Methyl red positive indicates:",
+    options: ["Neutral pH", "Acidic pH", "Alkaline pH", "No change"],
+    answer: 1
+  },
+  {
+    question: "Citrate utilization changes color to:",
+    options: ["Blue", "Red", "Yellow", "Green"],
+    answer: 0
+  },
+  {
+    question: "Albert stain identifies:",
+    options: ["E. coli", "Corynebacterium", "Staphylococcus", "Mycobacterium"],
+    answer: 1
+  },
+  {
+    question: "Acid-fast bacteria resist decolorization by:",
+    options: ["Water", "Alcohol", "HCl-alcohol", "Acetone"],
+    answer: 2
+  },
+  {
+    question: "Swarming is characteristic of:",
+    options: ["Proteus", "E. coli", "Klebsiella", "Staphylococcus"],
     answer: 0
   }
 ];
