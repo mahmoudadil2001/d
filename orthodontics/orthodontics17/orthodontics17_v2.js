d87a4c008a407690c40d7bd5f16f9eb5467ada14
mahmoud adil <mahmod.adil2001@gmail.com>
2026-04-22 23:18:16 +0300

    Update content for orthodontics17_v2.js

--- orthodontics/orthodontics17/orthodontics17_v2.js
+++ orthodontics/orthodontics17/orthodontics17_v2.js
@@ -1,7 +1,77 @@
 export const questions = [
   {
-    question: "Sample question for Orthodontics Lecture 17, Version 2?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
-    answer: 0
+    question: "What determines whether orthopedic forces produce skeletal vs dental effects?",
+    options: ["Force magnitude only", "Force direction/vector", "Patient's age", "Duration of wear"],
+    answer: 1
+  },
+  {
+    question: "Wearing orthopedic appliances in the evening/night is recommended because of increased release of?",
+    options: ["Thyroid hormone", "Cortisol", "Growth hormone", "Melatonin"],
+    answer: 2
+  },
+  {
+    question: "Which component of headgear is inserted into the buccal tube of the maxillary first permanent molar band?",
+    options: ["Outer bow", "Junction", "Inner bow", "Hook"],
+    answer: 2
+  },
+  {
+    question: "How far anteriorly should the inner bow of a face bow be placed from the maxillary incisors?",
+    options: ["1-2 mm", "4-5 mm", "7-8 mm", "10-12 mm"],
+    answer: 1
+  },
+  {
+    question: "Which headgear type produces a distal and inferior line of force action resulting in molar extrusion and backward rotation of the mandible?",
+    options: ["High vertical-pull headgear", "Occipital pull headgear", "Combination pull headgear", "Cervical pull headgear"],
+    answer: 3
+  },
+  {
+    question: "High vertical-pull headgear derives anchorage from which region?",
+    options: ["Occipital", "Cervical", "Parietal", "Frontal"],
+    answer: 2
+  },
+  {
+    question: "A disadvantage of combination pull headgear is?",
+    options: ["Less effective force", "Increased patient compliance challenges", "Higher risk of tooth damage", "Cannot be used with face bow"],
+    answer: 1
+  },
+  {
+    question: "Face mask is also known as all of the following EXCEPT?",
+    options: ["Reverse pull headgear", "Protraction headgear", "Distraction headgear", "Protraction face mask"],
+    answer: 2
+  },
+  {
+    question: "In face mask therapy, the force level recommended per side in primary and mixed dentitions is?",
+    options: ["100-200 gm", "300-500 gm", "600-800 gm", "900-1100 gm"],
+    answer: 1
+  },
+  {
+    question: "Face mask produces forward displacement of the maxilla by affecting which structures?",
+    options: ["Temporomandibular joint", "Circumnaxillary sutures", "Alveolar bone only", "Mandibular condyle"],
+    answer: 1
+  },
+  {
+    question: "Which intraoral appliance is commonly attached to the face mask via elastics?",
+    options: ["Lingual arch", "Palatal expansion appliance", "Bite plane", "Hawley retainer"],
+    answer: 1
+  },
+  {
+    question: "Chin cup appliance is primarily used to treat mandibular prognathism by?",
+    options: ["Stimulating mandibular growth", "Restraining forward growth of the mandible", "Moving maxilla forward", "Tipping upper incisors labially"],
+    answer: 1
+  },
+  {
+    question: "What is the recommended daily duration of wear for chin cup appliance to achieve desired results?",
+    options: ["4-6 hours", "8-10 hours", "12-16 hours", "20-24 hours"],
+    answer: 2
+  },
+  {
+    question: "Which effect is associated with chin cup appliance therapy?",
+    options: ["Labial tipping of lower incisors", "Increase in gonial angle", "Lingual tipping of lower incisors", "Increase in mandibular plane angle"],
+    answer: 2
+  },
+  {
+    question: "The outer bow of a face bow should rest how many mm away from the cheeks?",
+    options: ["Flush against cheeks", "1-2 mm", "Several mm", "10-15 mm"],
+    answer: 2
   }
 ];
