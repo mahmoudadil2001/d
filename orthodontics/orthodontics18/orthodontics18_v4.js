4b636a81eb15c1d835ca9bcee636ce1d16bc9a54
mahmoud adil <mahmod.adil2001@gmail.com>
2026-04-22 23:20:56 +0300

    Update content for orthodontics18_v4.js

--- orthodontics/orthodontics18/orthodontics18_v4.js
+++ orthodontics/orthodontics18/orthodontics18_v4.js
@@ -25,4 +25,53 @@ export const questions = [
     answer: 1
   },
   {
-    question: "Why is
+    question: "Why is the activator considered bulky and uncomfortable?",
+    options: ["It has too many wires", "It has large acrylic monoblock", "It requires headgear", "It is made of metal only"],
+    answer: 1
+  },
+  {
+    question: "The Bionator was developed in which country?",
+    options: ["Sweden", "United States", "Germany", "United Kingdom"],
+    answer: 2
+  },
+  {
+    question: "FR-IV Frankel appliance is indicated for?",
+    options: ["Class III malocclusion", "Class II division 1", "Bimaxillary protrusion and open bite", "Deep bite only"],
+    answer: 2
+  },
+  {
+    question: "Which functional appliance provides limited direct tooth control and has relatively slow treatment effects?",
+    options: ["Twin Block", "Herbst", "Frankel appliance", "Lip bumper"],
+    answer: 2
+  },
+  {
+    question: "The Twin Block appliance produces increase in all of the following EXCEPT?",
+    options: ["Mandibular length", "Incisor proclination", "Lower anterior facial height", "Upper incisor retroclination"],
+    answer: 3
+  },
+  {
+    question: "Oral screen is mainly used to treat which conditions?",
+    options: ["Class II malocclusion", "Oral habits such as thumb sucking and mouth breathing", "Mandibular deficiency", "Maxillary excess"],
+    answer: 1
+  },
+  {
+    question: "The lip bumper works by altering equilibrium between which structures?",
+    options: ["Upper and lower teeth", "Cheeks, lips, and tongue", "Maxilla and mandible", "Molars and premolars"],
+    answer: 1
+  },
+  {
+    question: "Which anterior component of the lip bumper separates lips and cheeks from teeth?",
+    options: ["Stainless steel wire", "Adjustment loops", "Acrylic or plastic shield", "Molar tubes"],
+    answer: 2
+  },
+  {
+    question: "The Herbst appliance produces continuous mandibular advancement during?",
+    options: ["Only during night time", "Only during swallowing", "All jaw movements", "Only during speaking"],
+    answer: 2
+  },
+  {
+    question: "An undesirable dental effect of the Herbst appliance can be?",
+    options: ["Retroclination of lower incisors", "Proclination of lower incisors", "Intrusion of molars", "Distalization of canines"],
+    answer: 1
+  }
+];
