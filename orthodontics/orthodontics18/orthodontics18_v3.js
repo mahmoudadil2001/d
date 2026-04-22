9e83a578ac7240e426a774f82cf6ad5cc2dda605
mahmoud adil <mahmod.adil2001@gmail.com>
2026-04-22 23:19:38 +0300

    Update content for orthodontics18_v3.js

--- orthodontics/orthodontics18/orthodontics18_v3.js
+++ orthodontics/orthodontics18/orthodontics18_v3.js
@@ -1,7 +1,77 @@
 export const questions = [
   {
-    question: "Sample question for Orthodontics Lecture 18, Version 3?",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    question: "Which phase of functional appliance treatment aims to reduce overjet and correct sagittal jaw relationship?",
+    options: ["Phase-1 treatment", "Phase-2 treatment", "Retention phase", "Fixed mechanotherapy phase"],
+    answer: 0
+  },
+  {
+    question: "Fixed functional appliances are attached to teeth and do NOT depend on patient compliance. Which is an example?",
+    options: ["Activator", "Bionator", "Twin Block", "Herbst appliance"],
+    answer: 3
+  },
+  {
+    question: "Which malocclusion can be treated with a reverse activator?",
+    options: ["Class I", "Class II division 1", "Class II division 2", "Class III"],
+    answer: 3
+  },
+  {
+    question: "A contraindication for activator includes severe proclination of which teeth?",
+    options: ["Upper incisors", "Lower incisors", "Upper canines", "Lower molars"],
+    answer: 1
+  },
+  {
+    question: "The success of the activator depends primarily on?",
+    options: ["Acrylic thickness", "Accuracy of the bite registration", "Number of clasps", "Patient's age only"],
+    answer: 1
+  },
+  {
+    question: "Which is NOT an advantage of the activator?",
+    options: ["Tissues not injured", "Helps eliminate abnormal habits", "Force on individual tooth can be controlled", "Oral hygiene is maintained"],
+    answer: 2
+  },
+  {
+    question: "The Bionator can be used for all of the following EXCEPT?",
+    options: ["Class II malocclusion", "Class III malocclusion", "Deep bite cases", "Gross intra-arch irregularities"],
+    answer: 3
+  },
+  {
+    question: "FR-II Frankel appliance is used for treating?",
+    options: ["Class I with deep bite", "Class II division 1 and division 2 malocclusion", "Class III malocclusion", "Bimaxillary protrusion"],
+    answer: 1
+  },
+  {
+    question: "Which Frankel appliance is used with headgear?",
+    options: ["FR-III", "FR-IV", "FR-V", "FR-II"],
+    answer: 2
+  },
+  {
+    question: "The Twin Block appliance consists of how many separate acrylic plates?",
+    options: ["One", "Two", "Three", "Four"],
+    answer: 1
+  },
+  {
+    question: "Full-time wear of Twin Block appliance is advised for approximately?",
+    options: ["3-6 months", "6-9 months", "9-12 months", "12-15 months"],
+    answer: 2
+  },
+  {
+    question: "Who introduced the oral screen?",
+    options: ["Newell", "Andresen", "Balter", "Clark"],
+    answer: 0
+  },
+  {
+    question: "The lip bumper is inserted into which part of molar bands for retention?",
+    options: ["Lingual sheaths", "Buccal tubes", "Occlusal rests", "Gingival hooks"],
+    answer: 1
+  },
+  {
+    question: "Which of the following is a use of the lip bumper?",
+    options: ["Treating tongue thrust only", "Space gaining in the lower arch", "Retracting upper incisors", "Expanding the maxilla"],
+    answer: 1
+  },
+  {
+    question: "The Herbst appliance was developed by whom?",
+    options: ["Emil Herbst", "William Clark", "Rolf Fränkel", "Wilhelm Balter"],
     answer: 0
   }
 ];
